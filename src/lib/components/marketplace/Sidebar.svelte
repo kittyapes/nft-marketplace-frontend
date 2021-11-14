92b7a73f3ad36278c8fe22c26c6363aa91b52c7a
Navindu <navindu@ids.company>
2021-11-15 00:02:09 +0530

    add: sidebar icons

--- src/lib/components/marketplace/Sidebar.svelte
+++ src/lib/components/marketplace/Sidebar.svelte
@@ -1,17 +1,18 @@
 <script>
 	import SidebarItem from '$lib/components/marketplace/SidebarItem.svelte';
 	import Back from '../icons/back.icon.svelte';
+	import Filters from '../icons/filters.icon.svelte';
 
 	import StatusFilter from '$lib/components/marketplace/StatusFilter.svelte';
 </script>
 
 <div class="w-72 h-screen fixed border-r border-gray-400 font-bold">
 	<div class="p-11 border-b border-gray-400 flex justify-between">
-		FILTER
+		<div class="flex flex-row items-center gap-3"><Filters /> FILTER</div>
 		<button><Back /></button>
 	</div>
 
-	<SidebarItem title="Status"><StatusFilter /></SidebarItem>
-	<SidebarItem title="Collection">sldkjf</SidebarItem>
-	<SidebarItem title="Price">sldkjf</SidebarItem>
+	<SidebarItem title="Status" iconSrc="/marketplace/status.svg"><StatusFilter /></SidebarItem>
+	<SidebarItem title="Collections" iconSrc="/marketplace/collections.svg">sldkjf</SidebarItem>
+	<SidebarItem title="Price" iconSrc="/marketplace/price.svg">sldkjf</SidebarItem>
 </div>
