a66a2c30d38ba3485d124abda0253c42f57b31c8
Navindu <navindu@ids.company>
2021-11-16 11:38:27 +0530

    add: load nfts from waifu db

---
+++ src/lib/sections/MarketplaceCardsSection.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Card from '$lib/components/marketplace/Card.svelte';
+	import { query } from 'svelte-apollo';
+	import { gql } from '@apollo/client/core';
+
+	const GET_CARDS = gql`
+		query ($numberToSkip: Int!) {
+			cards(first: 12, skip: $numberToSkip, orderBy: isAvailable, orderDirection: desc) {
+				id
+				amount
+				totalSupply
+				maxSupply
+				uri
+				creationDate
+			}
+		}
+	`;
+
+	const cards = query(GET_CARDS, {
+		variables: { numberToSkip: 0 }
+	});
+</script>
+
+<div class="flex flex-wrap mt-11 justify-center gap-6 cards">
+	{#if $cards.loading}
+		Loading..
+	{:else if $cards.data}
+		{#each $cards.data.cards as _card}
+			<Card uri={_card.uri} />
+		{/each}
+	{/if}
+</div>
