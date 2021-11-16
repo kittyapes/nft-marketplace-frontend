933b2c6e361be368ce1730101985ae20b219ce60
Navindu <navindu@ids.company>
2021-11-16 20:30:45 +0530

    ad: clickable status filter buttons

--- src/lib/components/marketplace/StatusFilter.svelte
+++ src/lib/components/marketplace/StatusFilter.svelte
@@ -1,5 +1,9 @@
+<script>
+	import StatusFilterButton from './StatusFilterButton.svelte';
+</script>
+
 <div class="flex flex-wrap gap-2">
-	<button class="w-28 py-3 border rounded-lg border-opacity-50 border-black">Buy Now</button>
-	<button class="w-28 py-3 border rounded-lg border-opacity-50 border-black">Has Offers</button>
-	<button class="w-28 py-3 border rounded-lg border-opacity-50 border-black">Auction</button>
+	<StatusFilterButton>Buy Now</StatusFilterButton>
+	<StatusFilterButton>Has Offers</StatusFilterButton>
+	<StatusFilterButton>Auction</StatusFilterButton>
 </div>
---
+++ src/lib/components/marketplace/StatusFilterButton.svelte
@@ -0,0 +1,16 @@
+<script>
+	let selected = false;
+
+	let updateSelected = () => {
+		selected = !selected;
+	};
+</script>
+
+<button
+	class="w-28 py-3 border-2 rounded-lg border-opacity-50 border-black bg-opacity-20 font-bold"
+	class:border-color-blue={selected}
+	class:bg-color-blue={selected}
+	on:click={updateSelected}
+>
+	<slot />
+</button>
