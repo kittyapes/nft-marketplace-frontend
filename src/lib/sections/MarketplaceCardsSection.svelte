b230ebcb0d61459a1cc855d03e08ca4c3439defd
Navindu <navindu@ids.company>
2021-11-18 10:51:48 +0530

    chore: remove unused commented code

--- src/lib/sections/MarketplaceCardsSection.svelte
+++ src/lib/sections/MarketplaceCardsSection.svelte
@@ -4,8 +4,8 @@
 	import { request, gql } from 'graphql-request';
 
 	const GET_CARDS = gql`
-		query {
-			cards(first: 12, skip: 0, orderBy: isAvailable, orderDirection: desc) {
+		query ($numberToSkip: Int!) {
+			cards(first: 12, skip: $numberToSkip, orderBy: isAvailable, orderDirection: desc) {
 				id
 				amount
 				totalSupply
@@ -16,22 +16,12 @@
 		}
 	`;
 
-	const cards: any = request('https://api.thegraph.com/subgraphs/name/hysmagus/waifu', GET_CARDS);
+	const cards: any = request('https://api.thegraph.com/subgraphs/name/hysmagus/waifu', GET_CARDS, {
+		numberToSkip: 0
+	});
 </script>
 
 <div class="flex flex-wrap mt-11 justify-center gap-6 cards">
-	<!-- {#if $cards.loading}
-		Loading..
-	{:else if $cards.data}
-		{#each $cards.data.cards as _card}
-			<Card uri={_card.uri} maxSupply={_card.maxSupply} />
-		{/each}
-	{/if} -->
-	<!-- {#await cards}
-		Loading....
-	{:then cardsData}
-		{JSON.stringify(cardsData)}
-	{/await} -->
 	{#await cards}
 		Loading....
 	{:then cardsData}
