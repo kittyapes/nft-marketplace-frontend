67d9f0802656b9a26656388b89a615068cc02a46
Jakub Bláha <jakub.blaha@gmail.com>
2021-11-19 16:42:24 +0100

    feat: V1 x HINATA select

--- src/routes/staking/_lib/Pill.svelte
+++ src/routes/staking/_lib/Pill.svelte
@@ -1,14 +1,34 @@
+<script>
+	export let mode = 'v1';
+</script>
+
 <div class="flex w-64 h-10 rounded-full overflow-hidden font-semibold">
-	<div class="w-1/2 bg-color-black text-white">V1</div>
-	<div class="w-1/2 border rounded-r-full border-[#969696] text-[#969696] ">HINATA</div>
+	<button class="rounded-l-full" on:click={() => (mode = 'v1')} class:selected={mode === 'v1'}
+		>V1</button
+	>
+	<button
+		class="rounded-r-full"
+		on:click={() => (mode = 'hinata')}
+		class:selected={mode === 'hinata'}
+	>
+		HINATA
+	</button>
 </div>
 
 <style>
-	div > div {
-		@apply grid place-items-center;
+	button {
+		@apply grid place-items-center w-1/2 transition duration-200;
+	}
+
+	button:not(.selected) {
+		@apply border border-[#969696] text-[#969696];
 	}
 
 	.flex {
 		filter: drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
+	}
+
+	.selected {
+		@apply bg-color-black text-white;
 	}
 </style>
