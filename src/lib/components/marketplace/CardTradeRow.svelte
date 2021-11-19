dfa77984e9470b31547561846eb132ee792c9d56
Navindu <navindu@ids.company>
2021-11-19 20:44:28 +0530

    add: Card Trade Tab

---
+++ src/lib/components/marketplace/CardTradeRow.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let idx = 0;
+</script>
+
+<div
+	class="w-full flex items-center gap-3 p-2"
+	class:bg-gray-100={idx % 2 == 0}
+	class:white={idx % 2 == 0}
+>
+	<div class="w-10 h-10 rounded-full bg-black " />
+	<div>
+		<div class="text-xs">Mura Masa</div>
+		<div class="text-sm">Offering <b>3.00 Ξ</b></div>
+	</div>
+</div>
--- src/lib/components/marketplace/CardTradeTab.svelte
+++ src/lib/components/marketplace/CardTradeTab.svelte
@@ -1,7 +1,39 @@
 <script>
 	import { fade } from 'svelte/transition';
+	import CardTradeRow from './CardTradeRow.svelte';
+	import RoundedButton from '../RoundedButton.svelte';
+
+	import { selectedCard } from '../../../../stores/marketplace';
 </script>
 
-<div in:fade={{ duration: 300 }} class="w-full h-full flex items-center justify-center">
-	Trading Coming Soon
+<div class="w-full h-full flex flex-col" in:fade={{ duration: 300 }}>
+	<div class="flex-grow h-auto flex-shrink overflow-y-scroll">
+		{#each Array(20).fill(0) as el, i}
+			<CardTradeRow idx={i} />
+		{/each}
+	</div>
+	<div class="w-full bottom-0 flex-shrink">
+		<div>
+			<div class="w-full border-t border-color-black border-opacity-30 flex">
+				<div class="w-1/2 border-r border-color-black border-opacity-30 pt-2">
+					<div class="text-sm text-color-black opacity-70 ">Current Offer</div>
+					<div class="text-2xl">3 Ξ</div>
+				</div>
+
+				<div class="w-1/2  pt-2 pl-4">
+					<div class="text-sm text-color-black opacity-70 ">Current Ask</div>
+					<div class="text-2xl">5 Ξ</div>
+				</div>
+			</div>
+
+			<div class="w-full mt-3 flex flex-row">
+				<div class="w-1/2 p-2">
+					<RoundedButton bgColor="from-color-purple to-color-blue">Sell</RoundedButton>
+				</div>
+				<div class="w-1/2 p-2">
+					<RoundedButton>List for sale</RoundedButton>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
