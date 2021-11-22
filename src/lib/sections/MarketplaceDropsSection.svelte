e843ff4681267e1f2755f4247286bb34fcee5a9c
Navindu <navindu@ids.company>
2021-11-22 22:49:20 +0530

    add: series drops component

--- src/lib/sections/MarketplaceDropsSection.svelte
+++ src/lib/sections/MarketplaceDropsSection.svelte
@@ -3,9 +3,12 @@
 	import AuctionDrop from '$lib/components/drops/AuctionDrop.svelte';
 	import LimitedDrop from '$lib/components/drops/LimitedDrop.svelte';
 	import RandomDrop from '$lib/components/drops/RandomDrop.svelte';
+	import SeriesDrop from '$lib/components/drops/SeriesDrop.svelte';
 </script>
 
 <div class="w-full">
+	<SeriesDrop />
+
 	<RandomDrop />
 	<div class="w-full h-1 border-b-2" />
 
