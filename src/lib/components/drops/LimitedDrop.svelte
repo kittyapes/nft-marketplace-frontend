e63ea3aa8a716feb243b4da2a1350c63b8adf4d9
Navindu <navindu@ids.company>
2021-11-22 12:45:24 +0530

    add: limited drop component

---
+++ src/lib/components/drops/LimitedDrop.svelte
@@ -0,0 +1,53 @@
+<div class="w-full flex h-auto">
+	<div class="w-1/3 flex items-center justify-center">
+		<div class="w-80 h-80 bg-gray-100 m-4 p-6">
+			<div class="w-full h-full flex items-center justify-center flex-col">
+				<div class="mt-2 w-full h-full aspect-w-1">
+					<img
+						src="https://databasewaifu.herokuapp.com/images/15"
+						alt="ape"
+						class="object-scale-down"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Content-->
+	<div class="flex-grow m-4 pl-4 border-l-2">
+		<div class="text-xs uppercase font-bold text-color-black opacity-70">AUCTION</div>
+		<div class="text-lg mt-1 font-bold text-color-black">The Pirate</div>
+
+		<div class="text-xs opacity-70 mt-6 text-color-black">CREATED BY</div>
+		<div class="text-base mt-1 font-bold flex items-center gap-2 text-color-purple">
+			<img
+				src="https://images.generated.photos/5Lc44DqxkR5Mhtm9SaQZgi8KBe6f0Lga7HkJAN54sGI/rs:fit:512:512/wm:0.95:sowe:18:18:0.33/czM6Ly9pY29uczgu/Z3Bob3Rvcy1wcm9k/LnBob3Rvcy92Ml8w/NTc4MDI3LmpwZw.jpg"
+				alt="avatar"
+				class="w-6 h-6 rounded-full"
+			/>
+			<span>@WAIFULover69</span>
+		</div>
+
+		<div class="text-xs opacity-70 mt-6 text-color-black">AUCTION ENDS IN</div>
+		<div class="text-base mt-1 font-bold text-color-black">1DAY 16H 4M 33S</div>
+
+		<div class="mt-4 w-full border-b-2" />
+
+		<div class="text-xs opacity-70 mt-2 text-color-black">INITIAL PRICE</div>
+		<div class="text-base font-bold text-color-black">2.1 Ξ</div>
+
+		<div class="mt-3 flex flex-row">
+			<div
+				class="w-36 border border-black rounded-full flex items-center justify-center p-2 text-sm m-px"
+			>
+				BID
+			</div>
+
+			<div
+				class="w-36 border rounded-full text-white  flex items-center justify-center p-2 text-sm -ml-9 bg-gradient-to-r from-color-purple to-color-blue"
+			>
+				$5500 | 2.1 Ξ
+			</div>
+		</div>
+	</div>
+</div>
