55c64c174b1f4c3ab65ceaf6a8128d5924822804
Jakub Bláha <jakub.blaha@gmail.com>
2021-11-23 23:48:24 +0100

    feat: profile button popup

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,21 +1,35 @@
 <script lang="ts">
-	export let variant: 'square' | 'rounded-black' = 'square';
+	export let variant: 'square' | 'rounded-black' | 'rounded-outline' = 'square';
 </script>
 
-<button class={variant}>
+<button class={variant} on:click>
 	<slot />
 </button>
 
 <style>
-	.square {
-		@apply bg-white uppercase font-semibold px-8 py-4;
-		border-radius: 0;
+	button {
+		@apply transition duration-200;
 		width: var(--width, 220px);
+		border-radius: var(--border-radius, 9999px);
+		padding-top: var(--py, 1rem);
+		padding-bottom: var(--py, 1rem);
+		padding-left: var(--px, 2rem);
+		padding-right: var(--px, 2rem);
+	}
+
+	button:active {
+		@apply transform scale-95;
+	}
+
+	.square {
+		@apply bg-white uppercase font-semibold rounded-none;
 	}
 
 	.rounded-black {
-		@apply bg-color-black uppercase font-semibold px-8 py-4 text-white;
-		border-radius: var(--border-radius, 9999px);
-		width: var(--width, 220px);
+		@apply bg-color-black uppercase font-semibold text-white;
+	}
+
+	.rounded-outline {
+		@apply border-2 border-color-black uppercase font-semibold;
 	}
 </style>
--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,6 +1,17 @@
-<!-- <script>
-	import Search from './Search.svelte';
-</script> -->
+<script>
+	// import Search from './Search.svelte';
+
+	import ProfilePopup from './ProfilePopup.svelte';
+
+	let displayProfilePopup = false;
+	let profileToggleButton;
+
+	function handleToggleClick(event) {
+		if (event.target === profileToggleButton) {
+			displayProfilePopup = !displayProfilePopup;
+		}
+	}
+</script>
 
 <div class="flex items-center h-16 px-8 gap-x-8 fixed w-full z-50 bg-white drop-shadow-lg">
 	<!-- Logo -->
@@ -34,13 +45,22 @@
 	</a> -->
 
 	<!-- Profile -->
-	<a href="/profile" class="relative font-semibold text-md whitespace-nowrap">Your Name</a>
+	<button
+		class="relative text-md font-semibold whitespace-nowrap"
+		on:click={handleToggleClick}
+		bind:this={profileToggleButton}
+	>
+		Your Name
+		{#if displayProfilePopup}
+			<ProfilePopup />
+		{/if}
+	</button>
 </div>
 
-<style>
-	/* #marketplace-link::after {
+<!-- <style>
+	#marketplace-link::after {
 		@apply absolute h-px w-full bg-gradient-to-r from-color-purple to-color-blue;
 		@apply left-0 bottom-0;
 		content: '';
-	} */
-</style>
+	}
+</style> -->
