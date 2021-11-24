b9f28c64bc9adbf6018557ba9645d9c433785af3
Navindu <navindu@ids.company>
2021-11-24 12:29:39 +0530

    feat: claim token ui

---
+++ src/lib/components/airdrop/ConnectWalletBanner.svelte
@@ -0,0 +1,19 @@
+<script>
+	import RoundedButton from '../RoundedButton.svelte';
+</script>
+
+<div
+	class="w-full max-w-3xl m-auto bg-black bg-opacity-5 border-4 border-black border-opacity-20 mt-12 py-11 px-7 rounded-2xl"
+>
+	<div
+		class="text-lg font-bold text-color-black flex flex-col md:flex-row items-center justify-center gap-4 md:gap-12"
+	>
+		Connect your wallet to check your eligibility
+
+		<div class="flex-grow-0">
+			<RoundedButton bgColor="from-color-purple to-color-blue">
+				<div class="px-8">Connect Wallet</div>
+			</RoundedButton>
+		</div>
+	</div>
+</div>
--- src/routes/airdrop/index.svelte
+++ src/routes/airdrop/index.svelte
@@ -1,6 +1,9 @@
 <script>
-	import RoundedButton from '$lib/components/RoundedButton.svelte';
+	import ClaimTokens from '$lib/components/airdrop/ClaimTokens.svelte';
+	import ConnectWalletBanner from '$lib/components/airdrop/ConnectWalletBanner.svelte';
 	import AirdropDistributionSection from '$lib/sections/AirdropDistributionSection.svelte';
+
+	let walletConnected = true;
 </script>
 
 <div class="w-full min-h-full px-6">
@@ -36,21 +39,11 @@
 			The Hinata community airdrop snapshot was taken on xxxx
 		</div>
 
-		<div
-			class="w-full max-w-3xl m-auto bg-black bg-opacity-5 border-4 border-black border-opacity-20 mt-12 py-11 px-7 rounded-2xl"
-		>
-			<div
-				class="text-lg font-bold text-color-black flex flex-col md:flex-row items-center justify-center gap-4 md:gap-12"
-			>
-				Connect your wallet to check your eligibility
-
-				<div class="flex-grow-0">
-					<RoundedButton bgColor="from-color-purple to-color-blue">
-						<div class="px-8">Connect Wallet</div>
-					</RoundedButton>
-				</div>
-			</div>
-		</div>
+		{#if walletConnected}
+			<ClaimTokens />
+		{:else}
+			<ConnectWalletBanner />
+		{/if}
 
 		<!-- Token Distribution -->
 		<div class="h-screen">
