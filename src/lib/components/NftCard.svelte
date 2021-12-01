c0cb2bcdd1d931e83fb36bffb253cdef8c320fe0
Jakub Bláha <jakub.blaha@gmail.com>
2021-12-01 11:16:41 +0100

    fix: icon import

--- src/lib/components/NftCard.svelte
+++ src/lib/components/NftCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { NftData } from '$lib/data/nft';
-	import Eth from '$icons/eth.icon.svelte';
+	import Eth from '$icons/eth.svelte';
 	import Heart from '$icons/heart.svelte';
 	import ThreeDots from '$icons/three-dots.svelte';
 
