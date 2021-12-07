2a581ffac19943a7bbb23899f1743ce879419393
gicharujohn <kittyapes@gmail.com>
2021-12-07 14:28:56 +0300

    feat: new phase one footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,34 +1,49 @@
 <div class="border-t h-80 border-[#1D1D1D4D]">
-	<div class="container mx-auto py-12 flex">
-		<div class="flex px-8 w-1/2">
-			<div><img src="/img/logo/footer-logo.svg" alt="" /></div>
-
-			<div class="grid grid-cols-2 gap-y-6 gap-x-8 ml-32 font-semibold h-[min-content]">
-				<a class="text-[#1D1D1D4D]" href="!#">MARKETPLACE</a>
-				<a href="!#">ABOUT</a>
-				<a href="!#">STAKING</a>
-				<a href="!#">BLOG</a>
+	<div class="container mx-auto py-12 flex justify-between">
+		<div class="flex flex-col px-8 w-1/4">
+			<div class="h-12">
+				<img src="/img/logo/logo.svg" class="h-full" alt="Hinata Logo" />
 			</div>
+			<div class="text-sm mt-3">The home of anime Web 3.0 content.</div>
 		</div>
 
-		<div class="grid grid-cols-2 gap-x-4">
-			<div class="perma-gray">Join our community:</div>
-			<div class="grid font-semibold gap-6">
-				<a href="!#">Discord</a>
-				<a href="!#">Twitter</a>
-				<a href="!#">Telegram</a>
-				<a href="!#" class="doc-link mt-8">Terms & Conditions</a>
+		<div class="w-3/4 flex justify-between items-start">
+			<div class="grid grid-cols-2 gap-x-4">
+				<div class="perma-gray">Join our community:</div>
+				<div class="grid font-semibold gap-2">
+					<a href="!#">Twitter</a>
+					<a href="!#">Discord</a>
+					<a href="!#">Telegram</a>
+					<a href="!#">Reddit</a>
+					<a href="!#">Medium</a>
+				</div>
+			</div>
+
+			<div class="grid grid-cols-2 gap-x-4 ml-8">
+				<div class="perma-gray">Company:</div>
+				<div class="grid font-semibold gap-2">
+					<a href="!#">About</a>
+					<a href="!#">Careers</a>
+				</div>
+			</div>
+
+			<div class="grid grid-cols-2 gap-x-4 ml-8">
+				<div class="perma-gray">Trade $HiNATA:</div>
+				<div class="grid font-semibold gap-2">
+					<a href="!#">Sushi</a>
+					<a href="!#">AscendEx</a>
+				</div>
 			</div>
 		</div>
+	</div>
 
-		<div class="grid grid-cols-2 gap-x-4 ml-8">
-			<div class="perma-gray">Latest news:</div>
-			<div class="grid font-semibold gap-6">
-				<a href="!#">Reddit</a>
-				<a href="!#">Medium</a>
-				<a href="!#">Uniswap</a>
-				<a href="!#" class="doc-link mt-8">Privacy Policy</a>
-			</div>
+	<!-- Copyright -->
+	<div class="container mx-auto flex items-center justify-center text-xs relative mt-8">
+		<a href="!#" class="doc-link">&copy; 2021 Hinata Foundation</a>
+		<div class="tos flex justify-center items-center w-auto absolute right-0">
+			<a href="!#" class="doc-link">Terms & Conditions</a>
+			<div class="w-[2px] h-4 bg-black mx-2 bg-opacity-50" />
+			<a href="!#" class="doc-link">Privacy Policy</a>
 		</div>
 	</div>
 </div>
