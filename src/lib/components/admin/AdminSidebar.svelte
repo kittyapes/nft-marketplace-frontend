e43a9ba9d7453baca27b2ede8a79f22a1bde7dc0
Navindu - IDS <navinu@ids.company>
2021-12-13 11:37:08 +0530

    add: admin page structure

---
+++ src/lib/components/admin/AdminSidebar.svelte
@@ -0,0 +1,28 @@
+<script>
+	import SidebarItem from '$lib/components/admin/AdminSidebarItem.svelte';
+	import Filters from '$icons/filters.svelte';
+</script>
+
+<div
+	class="w-full md:w-72
+	 h-auto md:h-screen border-t bg-white md:border-r border-gray-400 font-bold"
+>
+	<div class="py-4 px-5 border-b border-gray-400 flex justify-between">
+		<div class="flex flex-row items-center gap-3 uppercase">
+			<Filters />
+			Admin Dashboard
+		</div>
+	</div>
+
+	<div>
+		<SidebarItem title="Create" href="/admin/" iconSrc="/marketplace/status.svg" />
+
+		<SidebarItem
+			title="User management"
+			href="/admin/users"
+			iconSrc="/marketplace/collections.svg"
+		/>
+
+		<SidebarItem title="Analytics" href="/admin/analytics" iconSrc="/marketplace/price.svg" />
+	</div>
+</div>
