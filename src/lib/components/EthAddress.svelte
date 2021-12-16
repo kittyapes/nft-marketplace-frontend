7220c56d11e6402b1b7a9b16e83fe677135c782b
Navindu - IDS <navindu@ids.company>
2021-12-16 11:18:30 +0530

    feat: eth address copy to clipboard

--- src/lib/components/EthAddress.svelte
+++ src/lib/components/EthAddress.svelte
@@ -1,12 +1,18 @@
 <script>
 	import Copy from '$icons/copy.svelte';
+	import { notifySuccess } from '$utils/toast';
 
 	export let address = '';
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(address);
+		notifySuccess('Address Copied to Clipboard');
+	};
 </script>
 
 <div class="flex items-center gap-6">
 	<span class="text-color-blue font-bold">{address} </span>
-	<button>
+	<button on:click={copyToClipboard}>
 		<Copy />
 	</button>
 </div>
