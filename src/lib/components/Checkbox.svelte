7c82df5ad84b1b11517532b7f08c3b06beb1d361
Navindu - IDS <navindu@ids.company>
2021-12-16 12:45:13 +0530

    feat: checkbox label

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -1,1 +1,11 @@
-<input type="checkbox" class=" w-6 h-6 " />
+<script>
+	export let name = '';
+	export let checked = false;
+	export let id = 'check';
+	export let label = '';
+</script>
+
+<label class="flex items-center gap-4 {$$props.class}">
+	<input type="checkbox" class="w-6 h-6" {id} {name} {checked} />
+	{label}
+</label>
