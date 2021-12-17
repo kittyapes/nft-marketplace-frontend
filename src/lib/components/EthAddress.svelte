3af4c32648f5b3d3f725d32731ba9e523a982af0
Navindu - IDS <navindu@ids.company>
2021-12-17 13:03:20 +0530

    feat: concat ETH Addresses

--- src/lib/components/EthAddress.svelte
+++ src/lib/components/EthAddress.svelte
@@ -3,11 +3,18 @@
 	import { notifySuccess } from '$utils/toast';
 
 	export let address = '';
+	export let concat = false;
 
 	const copyToClipboard = () => {
 		navigator.clipboard.writeText(address);
 		notifySuccess('Address Copied to Clipboard');
 	};
+
+	const concatAddress = (id) => {
+		return `${id.substring(0, 6)}...${id.substring(id.length - 4, id.length)}`;
+	};
+
+	$: address = concat ? concatAddress(address) : address;
 </script>
 
 <div class="flex items-center gap-6">
