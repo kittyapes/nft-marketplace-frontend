f8d581c93772bf14556ac721facdbaf47909b58d
Jakub Bláha <jakub.blaha@gmail.com>
2021-12-17 19:02:42 +0100

    cleanup: login route

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
 	import { browser } from '$app/env';
-	import { login, requestLogin } from '$lib/api/login';
 	import SignMessagePopup from '$lib/components/AdminLoginPopup.svelte';
-	import { appSigner, currentUserAddress } from '$stores/wallet';
 	import { setPopup } from '$utils/popup';
-
-	$: if (browser && $currentUserAddress) {
-		// runLogin();
-	}
-
-	async function runLogin() {
-		const toSign = await requestLogin($currentUserAddress);
-
-		console.log('toSign', toSign);
-
-		const signature = await $appSigner.signMessage(toSign);
-
-		console.log('signed', signature);
-
-		const token = login($currentUserAddress, signature);
-	}
 
 	$: browser && setPopup(SignMessagePopup);
 </script>
