0a16f92b6b2ccffe37b0448bb9d5f66c8a2192c6
Navindu - IDS <navindu@ids.company>
2021-12-27 16:25:47 +0530

    refactor: move arrow icon

--- src/lib/components/admin/AdminSidebarItem.svelte
+++ src/lib/components/admin/AdminSidebarItem.svelte
@@ -2,6 +2,7 @@
 	import AnalyticsIcon from '$icons/analytics.svelte';
 	import UserManageIcon from '$icons/user-manage.svelte';
 	import CreateIcon from '$icons/create.svelte';
+	import RightArrowIcon from '../RightArrow.svelte';
 
 	export let title = '';
 	export let href = '';
@@ -23,21 +24,7 @@
 				{title}
 			</div>
 
-			<button>
-				<svg
-					width="14"
-					height="8"
-					viewBox="0 0 14 8"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-					class="-rotate-90"
-				>
-					<path
-						d="M2.49922 0.400391L6.99922 4.90039L11.4992 0.400391L13.2992 1.30039L6.99922 7.60039L0.699219 1.30039L2.49922 0.400391Z"
-						fill="#585858"
-					/>
-				</svg>
-			</button>
+			<button><RightArrowIcon /></button>
 		</div>
 	</a>
 </div>
