cfdf31e7706da6c0d0532bf44700b59e977be833
Jakub Bláha <jakub.blaha@gmail.com>
2021-12-28 16:54:48 +0100

    feat: edit profile loading indicator

--- src/routes/profile/edit.svelte
+++ src/routes/profile/edit.svelte
@@ -5,26 +5,18 @@
 	import Facebook from '$icons/socials/facebook.svelte';
 	import Twitter from '$icons/socials/twitter.svelte';
 	import Button from '$lib/components/Button.svelte';
-	import { slide } from 'svelte/transition';
+	import { fade, slide } from 'svelte/transition';
 	import Progressbar from '$lib/components/Progressbar.svelte';
 	import { writable } from 'svelte/store';
 	import { EditableProfileData, fetchProfileData, updateProfile } from '$utils/api/profile';
 	import { currentUserAddress } from '$stores/wallet';
 	import { notifyError } from '$utils/toast';
 	import { browser } from '$app/env';
+	import Loader from '$icons/loader.svelte';
 
-	const localDataStore = writable<EditableProfileData>({
-		username: '',
-		email: '',
-		bio: '',
-		socials: {
-			instagram: '',
-			facebook: '',
-			twitter: ''
-		}
-	});
+	const localDataStore = writable<EditableProfileData>(null);
 
-	$: usernameTaken = $localDataStore.username;
+	$: usernameTaken = $localDataStore?.username;
 
 	async function onSave() {
 		if (!$localDataStore.username) {
@@ -61,106 +53,118 @@
 
 	$: browser && $currentUserAddress && fetchAndDisplayProfile();
 	$: profileCompletionProgress =
+		$localDataStore &&
 		($localDataStore.username ? 1 : 0) * 25 +
-		($localDataStore.email ? 1 : 0) * 25 +
-		($localDataStore.bio ? 1 : 0) * 25;
+			($localDataStore.email ? 1 : 0) * 25 +
+			($localDataStore.bio ? 1 : 0) * 25;
 </script>
 
-<div class="bg-[#f2f2f2] py-16">
-	<div class="max-w-4xl mx-auto py-16 bg-white px-16">
-		<h1 class="uppercase text-center text-5xl font-semibold">
-			Edit Your <span class="gradient-text">Profile</span>
-		</h1>
+{#if $localDataStore}
+	<div class="bg-[#f2f2f2] py-16" in:fade>
+		<div class="max-w-4xl mx-auto py-16 bg-white px-16">
+			<h1 class="uppercase text-center text-5xl font-semibold">
+				Edit Your <span class="gradient-text">Profile</span>
+			</h1>
 
-		<div class="font-bold text-sm text-center mt-4">
-			Profile completion progress: <span class="gradient-text">{profileCompletionProgress}%</span>
-		</div>
+			<div class="font-bold text-sm text-center mt-4">
+				Profile completion progress: <span class="gradient-text">{profileCompletionProgress}%</span>
+			</div>
 
-		<Progressbar class="mt-2" value={profileCompletionProgress} />
+			<Progressbar class="mt-2" value={profileCompletionProgress} />
 
-		{#if profileCompletionProgress === 100}
-			<div class="px-16 mt-4">
-				<button
-					class="transition-btn
+			{#if profileCompletionProgress === 100}
+				<div class="px-16 mt-4">
+					<button
+						class="transition-btn
 				bg-gradient-to-r from-color-purple to-color-blue
 				text-white rounded-3xl font-semibold uppercase text-lg w-full
 				py-6 block
 				"
-				>
-					Claim your NFT
-				</button>
-			</div>
-		{:else}
-			<div class="font-bold gradient-text text-right mt-1 brightness-[90%]">Free NFT</div>
-		{/if}
+					>
+						Claim your NFT
+					</button>
+				</div>
+			{:else}
+				<div class="font-bold gradient-text text-right mt-1 brightness-[90%]">Free NFT</div>
+			{/if}
 
-		<div id="form-container" class="grid grid-cols-2 gap-y-6 mt-8">
-			<div>
-				<div>Add your username</div>
-				<div class="text-sm -mt-1">(Mandatory)</div>
-			</div>
+			<div id="form-container" class="grid grid-cols-2 gap-y-6 mt-8">
+				<div>
+					<div>Add your username</div>
+					<div class="text-sm -mt-1">(Mandatory)</div>
+				</div>
 
-			<div class="w-full">
-				<input
-					type="text"
-					class="input input-gray-outline"
-					placeholder="Username"
-					bind:value={$localDataStore.username}
-				/>
+				<div class="w-full">
+					<input
+						type="text"
+						class="input input-gray-outline"
+						placeholder="Username"
+						bind:value={$localDataStore.username}
+					/>
 
-				{#if usernameTaken}
-					<div class="text-xs text-red-500 mt-2 ml-1" transition:slide|local>
-						Username already taken
+					{#if usernameTaken}
+						<div class="text-xs text-red-500 mt-2 ml-1" transition:slide|local>
+							Username already taken
+						</div>
+					{/if}
+				</div>
+
+				<div>Add your email</div>
+				<input type="text" class="input input-gray-outline" placeholder="example@email.com" />
+
+				<div>Add your bio</div>
+				<TextArea outline placeholder="Enter your short bio" maxChars={200} />
+
+				<div>Upload a <br /> profile image</div>
+				<DragDropImage />
+
+				<div>Upload a <br /> background image</div>
+				<DragDropImage />
+
+				<div>Social links</div>
+				<div id="socials-container" class="grid gap-y-3">
+					<div>
+						<Instagram />
+						<input
+							type="text"
+							class="input input-gray-outline"
+							placeholder="Instagram link"
+							disabled
+						/>
 					</div>
-				{/if}
-			</div>
 
-			<div>Add your email</div>
-			<input type="text" class="input input-gray-outline" placeholder="example@email.com" />
+					<div>
+						<Facebook />
+						<input
+							type="text"
+							class="input input-gray-outline"
+							placeholder="Facebook link"
+							disabled
+						/>
+					</div>
 
-			<div>Add your bio</div>
-			<TextArea outline placeholder="Enter your short bio" maxChars={200} />
-
-			<div>Upload a <br /> profile image</div>
-			<DragDropImage />
-
-			<div>Upload a <br /> background image</div>
-			<DragDropImage />
-
-			<div>Social links</div>
-			<div id="socials-container" class="grid gap-y-3">
-				<div>
-					<Instagram />
-					<input
-						type="text"
-						class="input input-gray-outline"
-						placeholder="Instagram link"
-						disabled
-					/>
-				</div>
-
-				<div>
-					<Facebook />
-					<input
-						type="text"
-						class="input input-gray-outline"
-						placeholder="Facebook link"
-						disabled
-					/>
-				</div>
-
-				<div>
-					<Twitter />
-					<input type="text" class="input input-gray-outline" placeholder="Twitter link" disabled />
+					<div>
+						<Twitter />
+						<input
+							type="text"
+							class="input input-gray-outline"
+							placeholder="Twitter link"
+							disabled
+						/>
+					</div>
 				</div>
 			</div>
+
+			<Button rounded variant="rounded-black" stretch class="mt-12" on:click={onSave}>
+				Save changes
+			</Button>
 		</div>
-
-		<Button rounded variant="rounded-black" stretch class="mt-12" on:click={onSave}>
-			Save changes
-		</Button>
 	</div>
-</div>
+{:else}
+	<div class="py-64">
+		<Loader />
+	</div>
+{/if}
 
 <style>
 	#form-container > div {
