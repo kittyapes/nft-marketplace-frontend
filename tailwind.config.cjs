59643840ef3f715e3f0adbd64ffad23b4f126ade
Navindu - IDS <navindu@ids.company>
2021-12-29 19:01:37 +0530

    feat: gray and pink colors and variants

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,19 @@ const config = {
 		extend: {
 			colors: {
 				'color-black': '#1A1A1A',
+				'color-gray': {
+					lightest: '#F9F9F9',
+					lighter: '#F2F2F2',
+					light: '#D8D8D8',
+					base: '#4D4D4D',
+					dark: '#1D1D1DB2'
+				},
 				'color-red': '#D61717',
 				'color-purple': '#868BF7',
-				'color-blue': '#6CC7F8'
+				'color-blue': '#6CC7F8',
+				'color-pink': {
+					'dark': '#A95ABC'
+				}
 			}
 		}
 	},
