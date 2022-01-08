fc0665bccbf0a5ed7fd1c5da03f2abd59f528e3d
Jakub Bláha <jakub.blaha@gmail.com>
2022-01-08 15:49:47 +0100

    feat: display profile image on user profile

--- src/routes/profile/[address].svelte
+++ src/routes/profile/[address].svelte
@@ -3,6 +3,7 @@
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Copy from '$icons/copy.svelte';
+	import GuestUserAvatar from '$icons/guest-user-avatar.svelte';
 	import VerifiedBadge from '$icons/verified-badge.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import NftList from '$lib/components/NftList.svelte';
@@ -58,24 +59,12 @@
 <div class="mx-auto px-32 relative">
 	<div
 		class="border-white border-4 w-32 h-32 absolute top-0 transform -translate-y-1/2 rounded-full bg-white
-		grid place-items-center"
+		grid place-items-center shadow"
 	>
-		{#if false}
-			<img src="https://picsum.photos/id/237/200/200" class="rounded-full " alt="Profile avatar." />
+		{#if $localProfileData?.imageUrl}
+			<img src={$localProfileData?.imageUrl} class="rounded-full " alt="User avatar." />
 		{:else}
-			<svg
-				class="h-24 w-24 text-color-purple"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="1"
-					d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-				/>
-			</svg>
+			<GuestUserAvatar />
 		{/if}
 	</div>
 
