516c5789e929e76e216a4b320f62411c084fddee
gicharujohn <kittyapes@gmail.com>
2022-01-09 22:44:09 +0300

    feat: profile page redirect wallet check

--- src/routes/profile/index.svelte
+++ src/routes/profile/index.svelte
@@ -1,6 +1,12 @@
 <script>
+	import { browser } from '$app/env';
+
 	import { goto } from '$app/navigation';
 	import { currentUserAddress } from '$stores/wallet';
 
-	$: $currentUserAddress && goto('/profile/' + $currentUserAddress);
+	const noWalletConnectedFix = () => {
+		return browser && goto('/');
+	};
+
+	$: $currentUserAddress ? goto('/profile/' + $currentUserAddress) : noWalletConnectedFix();
 </script>
