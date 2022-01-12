29e8d20dc4d15bd6a7ca2822e19930d7e9555a48
kittyapes <kittyapes@gmail.com>
2022-01-12 14:20:25 +0300

    feat: staking contract updates

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -5,7 +5,7 @@ export const seedVestingMerkleDistributorLockContract =
 export const privateVestingMerkleDistributorLockContract =
 	'0xDa1bF624d0142E17EBF18c79a90d648D01e49F39';
 export const idoMerkleDistributorLockContract = '0x2016b695010039f7f7f5bEAAd21EDc17BDfBdfAF';
-export const stakingContract = '0xB451f2DE1CD013b7d6e45b245141DE51e077e192';
+export const stakingContract = '0xeFdd9CE1a71f7D6A9c9Fa5e3C2001A1eCE3aB64f';
 
 /*
 - staking - vesting contract
