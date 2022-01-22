7e3ad1c9940161455c6a83219e1b4d2fe93c4560
Jakub Bláha <jakub.blaha@gmail.com>
2022-01-22 19:22:44 +0100

    feat: loader component

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { browser } from '$app/env';
+	import LoaderGlow from '$icons/loader/loader-glow.svelte';
+	import LoaderIcon from '$icons/loader/loader-icon.svelte';
+
+	// Substract the navbar height from the loader height
+	export let compensateNavbar = true;
+
+	$: calculatedHeight = compensateNavbar
+		? `calc(${(browser && window?.innerHeight) || 0}px - 4rem)`
+		: `${browser && window.innerHeight}px`;
+</script>
+
+<div class="relative" style="height: {calculatedHeight}">
+	<div class="absolute grid place-items-center w-full" style="height: {calculatedHeight}">
+		<LoaderIcon />
+	</div>
+
+	<div
+		class="absolute grid place-items-center w-full animate-pulse"
+		style="height: {calculatedHeight}"
+	>
+		<div class="opacity-50">
+			<LoaderGlow />
+		</div>
+	</div>
+</div>
---
+++ src/routes/loader-test.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Loader from '$lib/components/Loader.svelte';
+</script>
+
+<Loader />
