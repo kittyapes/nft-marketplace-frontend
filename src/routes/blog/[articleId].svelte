32c416bdec2b7d83d71804fe5a9a873a8761c9e9
Jakub Bláha <jakub.blaha@gmail.com>
2022-01-30 10:14:46 +0100

    fix: blog post duplicate first image

--- src/routes/blog/[articleId].svelte
+++ src/routes/blog/[articleId].svelte
@@ -1,23 +1,29 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
 	import { blogPosts } from '$stores/blog';
 
 	const { articleId } = $page.params;
 
 	$: articleData = $blogPosts.filter((v) => v.segment === articleId)[0];
+
+	function fixContent(s: string) {
+		return s.replace(/<img.+?>/, '');
+	}
+
+	$: fixedContent = articleData && fixContent(articleData?.content);
 </script>
 
 {#if articleData}
-	<main class="container py-16 overflow-hidden mb-32">
-		<img src={articleData.thumbnail} alt="" class="w-full" />
+	<main class="container py-16 px-8 overflow-hidden mb-32 mx-auto">
+		<img src={articleData.thumbnail} alt="" class="w-full max-h-[500px] object-cover" />
 
 		<div class="grid grid-cols-2 mt-16 gap-x-16">
 			<div>
-				<h1 class="uppercase text-4xl">{articleData.title}</h1>
+				<h1 class="uppercase text-4xl italic font-light text-color-black">{articleData.title}</h1>
 			</div>
 
 			<div id="article-container" class="relative">
-				{@html articleData.content}
+				{@html fixedContent}
 			</div>
 		</div>
 	</main>
