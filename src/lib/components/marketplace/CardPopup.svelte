36b4acd0d7c094e84716b7a056db11156a484479
Jakub Bláha <jakub.blaha@gmail.com>
2022-02-06 11:59:42 +0100

    feat(nft-popup): share and fullscreen buttons

--- src/lib/components/marketplace/CardPopup.svelte
+++ src/lib/components/marketplace/CardPopup.svelte
@@ -7,6 +7,14 @@
 	import CardTradeTab from './CardTradeTab.svelte';
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/env';
+	import Fullscreen from '$icons/fullscreen.svelte';
+	import Share from '$icons/share.svelte';
+	import { notifySuccess } from '$utils/toast';
+
+	function handleShare() {
+		navigator.clipboard.writeText(window.location.href);
+		notifySuccess('Copied NFT link!');
+	}
 
 	let tab = 0;
 </script>
@@ -25,7 +33,7 @@
 	>
 		<!-- NFT Image side-->
 		<div class="w-full md:w-1/2 bg-gray-200 h-auto flex items-center justify-center">
-			<div class="m-10 text-center">
+			<div class="m-10 text-center h-full flex flex-col justify-end">
 				<div class=" w-72 h-72 flex items-center justify-center">
 					<img
 						src={$selectedCard?.image}
@@ -37,6 +45,21 @@
 				<!-- NFT Name and ID-->
 				<div class="font-bold text-lg mt-4 opacity-70">
 					{$selectedCard?.name} #{$selectedCard?.id}
+				</div>
+
+				<!-- Fullscreen and Share button -->
+				<div class="flex justify-center mt-24 mb-8 gap-x-4">
+					<a
+						href={$selectedCard?.image}
+						target="_blank"
+						class="transition-btn hover:brightness-110"
+					>
+						<Fullscreen />
+					</a>
+
+					<button class="transition-btn hover:brightness-110" on:click={handleShare}>
+						<Share />
+					</button>
 				</div>
 			</div>
 		</div>
