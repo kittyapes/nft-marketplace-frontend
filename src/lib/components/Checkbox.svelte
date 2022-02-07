0f16e756d03244746008f9f565272ed7a18c20c8
Jakub Bláha <jakub.blaha@gmail.com>
2022-02-07 20:51:16 +0100

    fix(user-management): checkbox appearance

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -1,11 +1,22 @@
 <script>
+	import Tick from '$icons/tick.svelte';
+
 	export let name = '';
 	export let checked = false;
 	export let id = 'check';
 	export let label = '';
 </script>
 
-<label class="flex items-center gap-4 {$$props.class}">
-	<input type="checkbox" class="w-6 h-6" {id} {name} {checked} />
+<label
+	class="flex items-center gap-4 {$$props.class} w-6 h-6 cursor-pointer
+	border rounded text-color-blue transition
+	{checked ? 'border-color-blue' : 'border-[#989898] border-opacity-40'}"
+>
+	<input type="checkbox" class="hidden" {id} {name} bind:checked />
+	{#if checked}
+		<div class="w-6 grid place-items-center">
+			<Tick />
+		</div>
+	{/if}
 	{label}
 </label>
