ef574ee0cee0c9c99247cac966c437dd00707e97
Jakub Bláha <jakub.blaha@gmail.com>
2022-02-10 20:30:54 +0100

    feat: prepare tabs

---
+++ src/routes/create/list/[listingType].svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import TabButton from '$lib/components/TabButton.svelte';
+	import CommonProperties from './_lib/CommonProperties.svelte';
+	import type { PropertyName } from './_lib/types';
+
+	const listingType = $page.params.listingType;
+	const tabs = ['Sale', 'Auction', 'Raffle', 'Limited Edition', 'Queue Drop', 'Gacha', 'Admin'];
+
+	const tabToProperties: { [key: string]: PropertyName[] } = {
+		Sale: ['price', 'date', 'quantity']
+	};
+
+	let selectedTab = tabs[0];
+</script>
+
+<div class="space-x-8">
+	{#each tabs as tab}
+		<TabButton selected={selectedTab === tab} on:click={() => (selectedTab = tab)}>{tab}</TabButton>
+	{/each}
+</div>
+
+<hr class="separator" />
+
+<h1 class="text-xl italic uppercase font-light mt-8">
+	Step 2: List for <span class="gradient-text">Sale</span>
+</h1>
+
+<hr class="separator mt-4" />
+
+<CommonProperties class="mt-8" propertyNames={tabToProperties[selectedTab]} />
---
+++ src/routes/create/list/_lib/CommonProperties.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import { keyBy } from 'lodash-es';
+
+	import type { PropertyName } from './types';
+
+	export let propertyNames: PropertyName[] = [];
+	export let propertyValues: { [key: string]: string } = {};
+
+	function is(propName: PropertyName) {
+		return propertyNames?.includes(propName);
+	}
+
+	$: console.log(propertyNames);
+</script>
+
+<div class={$$props.class}>
+	{#key propertyNames}
+		{#if is('price')}
+			Price
+		{/if}
+
+		{#if is('date')}
+			Price
+		{/if}
+
+		{#if is('quantity')}
+			Price
+		{/if}
+
+		{#if is('listingDate')}
+			Price
+		{/if}
+
+		{#if is('reservePrice')}
+			Price
+		{/if}
+
+		{#if is('auctionDate')}
+			Price
+		{/if}
+
+		{#if is('auctionDuration')}
+			Price
+		{/if}
+
+		{#if is('entryTickets')}
+			Price
+		{/if}
+
+		{#if is('ticketPrice')}
+			Price
+		{/if}
+
+		{#if is('totalTickets')}
+			Price
+		{/if}
+
+		{#if is('raffleDuration')}
+			Price
+		{/if}
+
+		{#if is('claimsBegin')}
+			Price
+		{/if}
+
+		{#if is('claimsDuration')}
+			Price
+		{/if}
+
+		{#if is('cutPrice')}
+			Price
+		{/if}
+
+		{#if is('preQueueOpens')}
+			Price
+		{/if}
+
+		{#if is('queueDuration')}
+			Price
+		{/if}
+
+		{#if is('gachaContract')}
+			Price
+		{/if}
+
+		{#if is('dateOfRelease')}
+			Price
+		{/if}
+
+		{#if is('initialPrice')}
+			Price
+		{/if}
+	{/key}
+</div>
