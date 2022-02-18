5d3b78af5e3cbfa292b2d7f83ff1f2d3a7aa7b5c
Jakub Bláha <jakub.blaha@gmail.com>
2022-02-18 16:47:14 +0100

    fix: card button hover effect

--- src/routes/create/_lib/CardButton.svelte
+++ src/routes/create/_lib/CardButton.svelte
@@ -1,7 +1,16 @@
-<button
+<!-- <button
 	on:click
-	class="rounded-xl border-2 border-[#1D1D1D4D] aspect-w-6 aspect-h-5 cursor-pointer
-    hover:border-color-purple transition grid place-items-stretch"
->
-	<slot />
-</button>
+	class=" border-2 border-[#1D1D1D4D] aspect-w-6 aspect-h-5 cursor-pointer
+    hover:border-color-purple transition "
+/> -->
+
+<div class="aspect-w-6 aspect-h-5">
+	<button
+		on:click
+		class="rounded-2xl p-1 cursor-pointer bg-[#DDDDDD] hover:bg-gradient-to-r from-color-purple to-color-blue"
+	>
+		<div class="bg-white h-full rounded-xl grid place-items-stretch">
+			<slot />
+		</div>
+	</button>
+</div>
