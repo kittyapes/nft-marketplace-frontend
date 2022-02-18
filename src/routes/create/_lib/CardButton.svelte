29468172eb940999d60e82cc3d444dfa9ac6dd5c
Jakub Bláha <jakub.blaha@gmail.com>
2022-02-18 16:47:35 +0100

    cleanup

--- src/routes/create/_lib/CardButton.svelte
+++ src/routes/create/_lib/CardButton.svelte
@@ -1,9 +1,3 @@
-<!-- <button
-	on:click
-	class=" border-2 border-[#1D1D1D4D] aspect-w-6 aspect-h-5 cursor-pointer
-    hover:border-color-purple transition "
-/> -->
-
 <div class="aspect-w-6 aspect-h-5">
 	<button
 		on:click
