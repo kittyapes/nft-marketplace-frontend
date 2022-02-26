e984742c43be79b806aacc0c87943f5f3e3b8f03
kittyapes <kittyapes@gmail.com>
2022-02-26 20:12:49 +0100

    feat: hover text on Listing type select

--- src/routes/create/choose-listing-format.svelte
+++ src/routes/create/choose-listing-format.svelte
@@ -1,30 +1,60 @@
 <script>
 	import { setPopup } from '$utils/popup';
 	import CardButton from '$lib/components/create/CardButton.svelte';
-	import DropType from '$lib/components/create/DropType.svelte';
+	import ListingType from '$lib/components/create/ListingType.svelte';
 	import ConfirmListingTypePopup from '$lib/components/create/ConfirmListingTypePopup.svelte';
+	import { fade } from 'svelte/transition';
 
-	const dropTypes = [
-		{ listingType: 'sale', title: 'Sale', imgUrl: '/img/create/drop-type-sale.svg' },
-		{ listingType: 'auction', title: 'Auction', imgUrl: '/img/create/drop-type-auction.svg' },
-		{ listingType: 'raffle', title: 'Raffle', imgUrl: '/img/create/drop-type-raffle.svg' }
+	const listingTypes = [
+		{
+			listingType: 'sale',
+			title: 'Sale',
+			imgUrl: '/img/create/drop-type-sale.svg',
+			hoverText: 'No description.'
+		},
+		{
+			listingType: 'auction',
+			title: 'Auction',
+			imgUrl: '/img/create/drop-type-auction.svg',
+			hoverText: 'Allow other users to make bids on your NFT.'
+		},
+		{
+			listingType: 'raffle',
+			title: 'Raffle',
+			imgUrl: '/img/create/drop-type-raffle.svg',
+			hoverText: 'No description'
+		}
 	];
 
 	function handleClick(dropType) {
 		setPopup(ConfirmListingTypePopup, { props: dropType });
 	}
+
+	let hoveredListingType = null;
 </script>
 
 <hr class="separator" />
 
 <h1 class="italic font-light uppercase mt-8 text-xl">Step 3: Choose listing format</h1>
 
-<p class="font-poppins text-[#585858] mt-8">Allow other users to make bids on your NFT</p>
+<div class="font-poppins text-[#585858] mt-8 h-6">
+	{#if hoveredListingType}
+		<div transition:fade={{ duration: 100 }}>
+			{listingTypes.find((v) => v.listingType === hoveredListingType)?.hoverText || ''}
+		</div>
+	{/if}
+</div>
 
 <div class="grid grid-cols-3 mt-8 gap-16 mb-64">
-	{#each dropTypes as dropType}
-		<CardButton on:click={() => handleClick(dropType)}>
-			<DropType imgUrl={dropType.imgUrl} title={dropType.title} />
+	{#each listingTypes as listingType}
+		<CardButton
+			on:click={() => handleClick(listingType)}
+			on:pointerenter={() => (hoveredListingType = listingType.listingType)}
+			on:pointerleave={() => {
+				if (hoveredListingType === listingType.listingType) hoveredListingType = null;
+			}}
+		>
+			<ListingType imgUrl={listingType.imgUrl} title={listingType.title} />
 		</CardButton>
 	{/each}
 </div>
