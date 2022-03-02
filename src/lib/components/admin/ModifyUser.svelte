0f00aa6f35655fecda606ee8eb46b97e584dd26d
kittyapes <kittyapes@gmail.com>
2022-03-02 21:02:18 +0100

    feat: integrate ModifyUser popup

---
+++ src/lib/components/admin/ModifyUser.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import EthAddress from '$lib/components/EthAddress.svelte';
+	import { postCreateAdmin } from '$utils/api/admin/adminManagement';
+	import { httpErrorHandler, notifySuccess } from '$utils/toast';
+	import Dropdown from '../Dropdown.svelte';
+	import Popup from '../Popup.svelte';
+
+	const adminTypeOptions = [{ label: 'Admin', value: 'admin' }];
+
+	export let userData: { name: string; address: string; role: string };
+
+	let name = userData?.name;
+
+	let isSubmiting = false;
+
+	async function submit() {
+		isSubmiting = true;
+
+		await postCreateAdmin(name, userData.address, 'admin')
+			.then(() => notifySuccess('Admin created!'))
+			.catch(httpErrorHandler);
+
+		isSubmiting = false;
+	}
+</script>
+
+<Popup class="min-w-[600px]" closeButton>
+	<div class="px-12 py-12">
+		<div class="w-full text-center text-5xl text-color-black uppercase">
+			Modify
+			<span class="text-transparent bg-clip-text bg-gradient-to-r from-color-purple to-color-blue">
+				Admin
+			</span>
+		</div>
+
+		<div class="mt-16 text-color-black grid grid-cols-3 gap-y-6">
+			<div class=" font-bold uppercase">Edit Username</div>
+			<input placeholder="Account name" class="input col-span-2" value={name} />
+
+			<div class="font-bold uppercase">Address</div>
+			<EthAddress class="col-span-2" address="0x3468C6dE9662C2877vd10184B4228e5711b89D42" concat />
+
+			<div class="font-bold uppercase">Role</div>
+			<div class="col-span-2">
+				<Dropdown options={adminTypeOptions} />
+			</div>
+
+			<!-- <div class="font-bold uppercase">Permissions</div> -->
+			<!-- <div class="max-w-sm grid grid-cols-2 grid-rows-3 gap-4 col-span-2"> -->
+			<!-- {#each permissions as permission}
+						<Checkbox label={permission} />
+					{/each} -->
+			<!-- </div> -->
+		</div>
+
+		<div class="flex mt-8 justify-center px-16">
+			<button class="btn-primary" disabled={isSubmiting} on:click={submit}>Save Changes</button>
+		</div>
+	</div>
+</Popup>
--- src/lib/sections/admin/AdministratorsSection.svelte
+++ src/lib/sections/admin/AdministratorsSection.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
 	import EthAddress from '$lib/components/EthAddress.svelte';
 	import PersonIcon from '$icons/person.svelte';
-	import ModifyAdmin from '$lib/components/admin/ModifyAdmin.svelte';
+	import ModifyUser from '$lib/components/admin/ModifyUser.svelte';
+	import { setPopup } from '$utils/popup';
 
-	let modifyAdminOpen = false;
+	function handleModifyAdmin(userData) {
+		setPopup(ModifyUser, { props: { userData } });
+	}
 </script>
 
 <div class="mt-32">
-	{#if modifyAdminOpen}
-		<ModifyAdmin on:close={() => (modifyAdminOpen = false)} />
-	{/if}
-
 	<div class="w-full flex justify-between items-baseline">
 		<div class="uppercase text-lg font-bold ">Administrators</div>
 		<a
@@ -23,7 +22,7 @@
 
 	<div class="max-h-[900px] overflow-auto custom-scrollbar mt-5 pb-4">
 		<table class="w-full table table-auto border-t border-color-black border-opacity-30">
-			{#each [{ address: '0x40D6f8Ac990d98F9c812A3910e3255345fB32f8e', username: 'Jakub', status: 'Superadmin' }] as row}
+			{#each [{ address: '0x40D6f8Ac990d98F9c812A3910e3255345fB32f8e', name: 'Jakub', role: 'admin' }] as row}
 				<tr class="h-20 border-b border-color-black border-opacity-30">
 					<td class="px-4">
 						<div class="flex items-center gap-4">
@@ -34,18 +33,18 @@
 
 					<td class="px-6">
 						<span class="font-bold">Username:</span>
-						{row.username}
+						{row.name}
 					</td>
 
 					<td class="px-6">
 						<span class="font-bold">Status:</span>
-						{row.status}
+						{row.role}
 					</td>
 
 					<td class="px-4 w-28 whitespace-nowrap">
 						<button
 							class="btn btn-rounded uppercase italic btn-gradient h-12 w-48 font-light"
-							on:click={() => (modifyAdminOpen = true)}
+							on:click={() => handleModifyAdmin(row)}
 						>
 							Modify
 						</button>
