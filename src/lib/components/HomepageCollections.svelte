4eff6142521cf973552d30b071718d21b99d1b4b
PavelLukl <pavellukl@ids.company>
2022-03-22 08:52:22 +0100

    backup

--- src/lib/components/HomepageCollections.svelte
+++ src/lib/components/HomepageCollections.svelte
@@ -1,3 +1,16 @@
 <script lang='ts'>
 
 </script>
+
+<div class='grid place-items-center w-full'>
+    <div class="w-5/6 flex flex-col">
+        <div class="flex flex-col max-w-min gap-4 my-20">
+            <h1 class='uppercase font-bold text-7xl'>Collections</h1>
+            <p class='font-semibold text-xl'>The top NFT collections on Hinata, ranked by floor price, volume and other statistics.</p>
+        </div>
+        <hr class='border-t border-black border-opacity-30'>
+        <div class=''>
+            
+        </div>
+    </div>
+</div>
---
+++ src/routes/test.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import HomepageCollections from "$lib/components/HomepageCollections.svelte";
+</script>
+
+<HomepageCollections></HomepageCollections>
