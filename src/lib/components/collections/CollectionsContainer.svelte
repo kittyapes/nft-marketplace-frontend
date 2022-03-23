fbff9b47ea5fd4c366ead239463f2e53cd260f4d
PavelLukl <pavellukl@ids.company>
2022-03-23 21:41:59 +0100

    add: collections display

--- src/lib/components/collections/CollectionsContainer.svelte
+++ src/lib/components/collections/CollectionsContainer.svelte
@@ -1,29 +1,88 @@
-<script lang='ts'>
-    import type { CollectionData } from "src/interfaces/collectionData";
+<script lang="ts">
+	import Eth from '$icons/eth.svelte';
+	import type { CollectionData } from 'src/interfaces/collectionData';
+	import VerifiedBadge from '$icons/verified-badge.svelte';
+	import ArrowDown from '$icons/arrow-down.svelte';
+	import { seperateNumberWithCommas } from '$utils/misc/seperateNumberWithSeparators';
 
-    export let collections: CollectionData[];
+	export let collections: CollectionData[];
+
+	let selection: 'ALL' | 'VERIFIED' = 'ALL';
 </script>
 
-<div class='w-full flex flex-col'>
-    <div class="flex">
-        <div class=""></div>
-        <div class="bg-color-gray-lighter w-full grid grid-cols-[1fr_7fr_1.5fr_3fr_2fr_3fr]">
-            {#each collections as collection, i}
-                <div class='border-t border-black border-opacity-[0.15]'>
-                    <div class='w-[95%]'>
-                        <div class='text-center text-sm grid place-items-center'>{i + 1}</div>
-                        <div class='flex'>
-                            <div class='w-10 h-10 background profile-pic bg-cover rounded-full  ' style="--url: url({collection.imageUrl})" ></div>
-                        </div>
-                    </div>
-                </div>
-            {/each}
-        </div>  
-    </div>
+<div class="w-full flex flex-col gap-10">
+	<div class="flex font-semibold text-xs gap-0">
+		{#if selection === 'ALL'}
+			<button class="uppercase btn btn-gradient w-28 h-12">all</button>
+			<button class="uppercase btn bg-color-gray-lighter w-28 h-12">verified</button>
+		{:else}
+			<button class="uppercase btn bg-color-gray-lighter w-28 h-12">all</button>
+			<button class="uppercase btn btn-gradient w-28 h-12">verified</button>
+		{/if}
+	</div>
+	<div class="bg-color-gray-lighter w-full flex flex-col">
+		<div
+			class="grid w-[95%] grid-cols-[8.5fr_1.5fr_3fr_2fr_3fr] text-center text-xs text-color-gray-base py-5 font-bold"
+		>
+			<div />
+			<div class="uppercase">Floor</div>
+			<div class="uppercase">Total vol</div>
+			<div class="uppercase">24h vol</div>
+			<div class="uppercase">24h vol %</div>
+		</div>
+		{#each collections as collection, i}
+			<div class="grid w-full py-4 border-t border-black border-opacity-[0.15]">
+				<div class="grid grid-cols-[1.5fr_7fr_1.5fr_3fr_2fr_3fr] w-[95%] place-items-center">
+					<div class="text-center text-sm grid place-items-center">{i + 1}</div>
+					<div class="flex place-items-center gap-6 place-self-start">
+						<div
+							class="w-12 h-12 background profile-pic bg-cover rounded-full"
+							style="--url: url({collection.imageUrl})"
+						/>
+						<div class="flex font-semibold text-sm gap-2">
+							{collection.name}
+							{#if collection.verfified}
+								<VerifiedBadge />
+							{/if}
+						</div>
+					</div>
+					<div class="flex place-items-center gap-2 text-sm">
+						<Eth />
+						{seperateNumberWithCommas(Math.round((collection.floor + Number.EPSILON) * 100) / 100)}
+					</div>
+					<div class="flex place-items-center gap-2 text-sm">
+						<Eth />
+						{seperateNumberWithCommas(
+							Math.round((collection.totalVolume + Number.EPSILON) * 100) / 100
+						)}
+					</div>
+					<div class="flex place-items-center gap-2 text-sm">
+						<Eth />
+						{seperateNumberWithCommas(
+							Math.round((collection.dailyVolume + Number.EPSILON) * 100) / 100
+						)}
+					</div>
+					<div
+						class="flex place-items-center text-sm text-color-green"
+						class:text-color-red={collection.dailyGrowth < 0}
+					>
+						{(Math.round((collection.dailyGrowth + Number.EPSILON) * 100) / 100).toLocaleString()}%
+						<div class:rotate-180={collection.dailyGrowth < 0}>
+							<ArrowDown />
+						</div>
+					</div>
+				</div>
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style lang="postcss">
 	.profile-pic {
 		background-image: var(--url);
+	}
+
+	.selected {
+		@apply text-white;
 	}
 </style>
--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-    import HomepageCollections from "$lib/components/collections/HomepageCollections.svelte";
-    import type { CollectionData } from "src/interfaces/collectionData";
-    
-    let collections = Array(20).fill(0)
-        .map(() => ({
-                name: 'Terraforms', 
-                imageUrl: 'https://picsum.photos/400', 
-                origin: 'HINATA',
-                verfified: true,
-                floor: 0.5,
-                totalVolume: 2762839.02,
-                dailyVolume: 16430.58,
-                dailyGrowth: Math.random() < 0.5 ? (Math.random() + 1) * -10 * 50 : (Math.random() + 1) * 10 * 50,
-                } as CollectionData)
-            );
+	import HomepageCollections from '$lib/components/collections/HomepageCollections.svelte';
+	import type { CollectionData } from 'src/interfaces/collectionData';
+
+	let collections = Array(20)
+		.fill(0)
+		.map(
+			() =>
+				({
+					name: 'Terraforms',
+					imageUrl: 'https://picsum.photos/400',
+					origin: 'HINATA',
+					verfified: Math.random() < 0.5,
+					floor: 0.5,
+					totalVolume: 2762839.02,
+					dailyVolume: 16430.58,
+					dailyGrowth: Math.random() < 0.5 ? (Math.random() + 1) * -10 : (Math.random() + 1) * 10
+				} as CollectionData)
+		);
 </script>
 
-<HomepageCollections {collections}></HomepageCollections>
+<HomepageCollections {collections} />
