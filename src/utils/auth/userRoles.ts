a29a25f1a26c4b78b4f162a0ebedea7de09ca634
kittyapes <kittyapes@gmail.com>
2022-04-06 17:26:09 +0200

    fix: users without the admin/superadmin role could access the admin pages

--- src/utils/auth/userRoles.ts
+++ src/utils/auth/userRoles.ts
@@ -6,7 +6,10 @@ import { derived } from 'svelte/store';
  * A list of roles the current user has.
  */
 export const userRoles = derived(profileData, (data: UserData) => {
-	if (!data) return [];
+	if (!data) return null;
+
+	// No roles, API doesn't return an empty array.
+	if (!data.roles) return [];
 
 	// Returning an array because API currently returns a string.
 	return [data.roles];
@@ -14,13 +17,12 @@ export const userRoles = derived(profileData, (data: UserData) => {
 
 /**
  * Store containing a function that will check whether the current user has
- * at least one of the supplied roles.
+ * at least one of the supplied roles. This is in a store so we can use it reactively.
  */
-export const userHasRole = derived<typeof userRoles, (...role: UserRole[]) => boolean>(
-	userRoles,
-	(roles) => {
-		return (...role: UserRole[]) => {
-			return role.some((r) => roles.includes(r));
-		};
-	}
-);
+export const userHasRole = derived<typeof userRoles, (...role: UserRole[]) => boolean>(userRoles, (roles) => {
+	return (...role: UserRole[]) => {
+		if (!roles) return false;
+
+		return role.some((r) => roles.includes(r));
+	};
+});
