4d554c0ba8ac6a50ce58c5b19bf7b6da8db38e95
PavelLukl <pavellukl@ids.company>
2022-04-17 08:15:51 +0200

    add: search bar

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,5 +1,5 @@
 <script>
-	// import Search from './Search.svelte';
+	import Search from './Search.svelte';
 	import ProfilePopup from './ProfilePopup.svelte';
 	import { connectToWallet } from '$utils/wallet/connectWallet';
 	import { appSigner } from '$stores/wallet';
@@ -35,6 +35,7 @@
 	</a>
 
 	<!-- <Search /> -->
+	<Search />
 
 	<!-- Flex filler -->
 	<div class="flex-grow" />
