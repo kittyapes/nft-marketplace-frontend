30e6c7aa5509b711ebc4b688d4561f26743dc762
PavelLukl <pavellukl@ids.company>
2022-04-19 19:06:32 +0200

    backup

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,13 +1,22 @@
-<script>
+<script lang="ts">
 	import Search from '$icons/search.svelte';
+	import { debounce } from 'lodash-es';
+
+	let query;
+	let beginSearch = false;
+
+	const debouncedSearch = debounce(async () => {
+		beginSearch = true;
+	}, 500);
+
+	$: if (query) {
+		debouncedSearch();
+		console.log(beginSearch);
+	}
 </script>
 
-<div class="flex py-2 px-4 items-center gap-x-4 border border-black border-opacity-30 rounded-md">
+<div class="flex py-2 px-4 items-center gap-x-4 border border-black border-opacity-30 rounded-md flex-grow">
 	<Search />
 
-	<input
-		type="text"
-		class="w-72 focus:outline-none"
-		placeholder="Search nfts, collections, and artists"
-	/>
+	<input bind:value={query} type="text" class="w-72 focus:outline-none" placeholder="Search nfts, collections, and artists" />
 </div>
