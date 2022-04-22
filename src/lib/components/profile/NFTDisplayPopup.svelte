fa1d70f9008f7de8c839427ba8137f6f5e522fff
PavelLukl <pavellukl@ids.company>
2022-04-22 18:57:06 +0200

    fix: don't display make offer on own profile page

--- src/lib/components/profile/NFTDisplayPopup.svelte
+++ src/lib/components/profile/NFTDisplayPopup.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { selectedCard } from '$stores/marketplace';
-
+	import { currentUserAddress } from '$stores/wallet';
+	import { toLower } from 'lodash-es';
 	import type { TokenData } from 'src/interfaces/tokenData';
 	import InfoTab from '../marketplace/UniversalPopup/InfoTab.svelte';
 	import PopupContainer from '../marketplace/UniversalPopup/PopupContainer.svelte';
@@ -17,12 +18,15 @@
 		description: data.metadata.description
 	};
 
-	console.log(data);
+	console.log($currentUserAddress.toLowerCase());
+	console.log(data.owner_of);
 </script>
 
 <PopupContainer closeButton on:close>
 	<div slot="content" class="flex-grow flex flex-col">
 		<InfoTab data={infoData} />
-		<button class="btn btn-rounded btn-black uppercase text-sm font-normal mt-auto w-full mb-5 self-center">make an offer</button>
+		{#if data.owner_of !== $currentUserAddress.toLowerCase()}
+			<button class="btn btn-rounded btn-black uppercase text-sm font-normal mt-auto w-full mb-5 self-center">make an offer</button>
+		{/if}
 	</div>
 </PopupContainer>
