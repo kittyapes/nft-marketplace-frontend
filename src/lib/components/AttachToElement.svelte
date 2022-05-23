672737462c45a5365fa5d0ff6a3de48ff833524b
kittyapes <kittyapes@gmail.com>
2022-05-23 11:37:27 +0200

    feat: stats table and round buttons

---
+++ src/lib/components/AttachToElement.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { browser } from '$app/env';
+
+	export let to: HTMLElement;
+	export let right = false;
+	export let bottom = false;
+	export let offsetX = 0;
+	export let offsetY = 0;
+
+	$: clientRect = browser && to?.getBoundingClientRect();
+</script>
+
+<div
+	class="absolute"
+	style="
+    top: {clientRect?.top + (bottom ? clientRect?.height : 0) + offsetX}px;
+    left: {clientRect?.left + (right ? clientRect?.width : 0) + offsetY}px"
+>
+	<slot />
+</div>
--- src/routes/collection/[collectionId]/index.svelte
+++ src/routes/collection/[collectionId]/index.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import type { UserData } from '$interfaces/userData';
+	import ActionMenu from '$lib/components/ActionMenu.svelte';
+	import AttachToElement from '$lib/components/AttachToElement.svelte';
+	import Dropdown from '$lib/components/Dropdown.svelte';
 	import { currentUserAddress } from '$stores/wallet';
 	import { apiGetCollection, Collection } from '$utils/api/collection';
 	import { fetchProfileData } from '$utils/api/profile';
@@ -17,6 +20,23 @@
 	}
 
 	$: $currentUserAddress && fetchCollectionData();
+
+	const collectionStats = [
+		['Highest Sale', '$0.2M'],
+		['Floor Price', '100 ETH'],
+		['Total Volume', '$3.5B'],
+		['Items', '9.8K'],
+		['Owners', '6.5K'],
+		['24hr Volume', '$12.4M']
+	];
+
+	const collectionMenuButtonOptions = [
+		{ label: 'Claim Ownership', action: () => {} },
+		{ label: 'Report', action: () => {} }
+	];
+
+	let menuButton: HTMLButtonElement;
+	let showCollectionMenu = false;
 </script>
 
 <main class="max-w-screen-lg mx-auto px-16">
@@ -48,13 +68,42 @@
 
 	<!-- Creator username and address -->
 	<div class="flex justify-center items-center space-x-3 mt-2">
-		<div class="text-color-gradient text-xl max-w-max font-medium">@{creatorData?.username}</div>
+		<div class="text-color-gradient text-xl max-w-max font-medium font-poppins">@{creatorData?.username}</div>
 		<div class="bg-[#F5F5F5] flex rounded-full px-4 py-2 space-x-2">
 			<img class="w-5" src="/svg/icons/collection-gradient-eth.svg" alt="Ethereum." />
 			<div class="font-mono text-[#6E6E6E] text-sm">{shortenAddress(creatorData?.address)}</div>
 		</div>
 	</div>
+
+	<!-- Stats table -->
+	<div class="border border-black h-24 flex rounded-lg justify-evenly mt-8">
+		{#each collectionStats as [stat, value]}
+			<div class="flex flex-col items-center justify-center border-r border-black last:border-0 w-full">
+				<div class="text-sm">{stat}</div>
+				<div class="text-xl font-semibold mt-1">{value}</div>
+			</div>
+		{/each}
+	</div>
+
+	<!-- Upload and menu buttons -->
+	<div class="flex space-x-4 mt-6 justify-center">
+		<!-- Upload button -->
+		<button class="btn rounded-full border border-black w-14 h-14 grid place-items-center">
+			<img src="/svg/icons/collection-upload.svg" alt="Upload." />
+		</button>
+
+		<!-- Menu button -->
+		<button class="btn rounded-full border border-black w-14 h-14 grid place-items-center" bind:this={menuButton} on:click={() => (showCollectionMenu = !showCollectionMenu)}>
+			<img src="/svg/icons/collection-menu.svg" alt="Upload." />
+		</button>
+	</div>
 </main>
+
+{#if showCollectionMenu}
+	<AttachToElement to={menuButton} bottom right>
+		<ActionMenu options={collectionMenuButtonOptions} on:optionClick={() => (showCollectionMenu = false)} />
+	</AttachToElement>
+{/if}
 
 <pre>
     {$page.params.collectionId}
