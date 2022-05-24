36317755df2f5b0b71cec8bfa7170107746fa725
PavelLukl <pavellukl@ids.company>
2022-05-24 11:03:59 +0200

    feat: 403 page

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -13,9 +13,8 @@
 </script>
 
 <script lang="ts">
-	import Error403 from '$icons/error403.svelte';
-
 	import Error404 from '$icons/error404.svelte';
+	import Error403Page from '$lib/components/Error403Page.svelte';
 	import LoadedContent from '$lib/components/LoadedContent.svelte';
 	import { onMount } from 'svelte';
 
@@ -29,19 +28,13 @@
 <LoadedContent {loaded}>
 	{#if response === 404}
 		<div class="h-full w-full grid place-items-center">
-			<div class="flex flex-col gap-10 p-20 items-center">
+			<div class="flex flex-col gap-10 p-40 items-center">
 				<Error404 />
 				<h1 class="text-7xl py-4 font-semibold text-color-gray-base">Something's missing.</h1>
 				<button class="btn btn-rounded gradient-text font-semibold text-5xl" on:click={() => window.history.back()}>Go Back</button>
 			</div>
 		</div>
 	{:else if response === 403}
-		<div class="h-full w-full grid place-items-center">
-			<div class="flex flex-col gap-10 p-20 items-center">
-				<Error403 />
-				<h1 class="text-7xl py-4 font-semibold text-color-gray-base">Access Denied</h1>
-				<button class="btn btn-rounded gradient-text font-semibold text-5xl" on:click={() => window.history.back()}>Go Back</button>
-			</div>
-		</div>
+		<Error403Page />
 	{/if}
 </LoadedContent>
