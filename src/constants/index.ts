1b078a702c22f177efba2698d89483d966b61177
PavelLukl <pavellukl@ids.company>
2022-05-27 19:29:18 +0200

    fix: make image/webp an accepted video file type

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
-export const acceptedImages = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'image/webp'];
-export const acceptedVideos = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'video/mp4', 'video/webp'];
+export const acceptedImages = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif' ];
+export const acceptedVideos = ['image/png', 'image/jpeg', 'image/jpg', 'image/gif', 'video/mp4', 'image/webp'];
 export const acceptedNftFileTypes = acceptedImages.concat(acceptedVideos);
 
 export const walletConnectionRequiredRoutes = ['/profile/edit', '/create'];
