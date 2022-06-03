a9cb8abbc84feeb2f2df7762a7d654199a4deca9
kittyapes <kittyapes@gmail.com>
2022-06-03 11:14:36 +0200

    fix: favorite nfts error

--- src/routes/profile/[address].svelte
+++ src/routes/profile/[address].svelte
@@ -121,12 +121,16 @@
 	const fetchFavoriteNfts = async (address: string) => {
 		const favorites = await getUserFavoriteNfts(address);
 		console.log(favorites);
-		favoriteNfts = await Promise.all(favorites.map((f) => adaptNftDataNftCard(f.nft)));
+		favoriteNfts = favorites && (await Promise.all(favorites.map((f) => adaptNftDataNftCard(f.nft))));
 	};
 
 	onMount(() => {
-		fetchCreatedNfts() && fetchActiveListing() && fetchFavoriteNfts(address);
+		fetchCreatedNfts() && fetchActiveListing();
 	});
+
+	$: if ($currentUserAddress) {
+		fetchFavoriteNfts($currentUserAddress);
+	}
 </script>
 
 <div class="h-72 bg-color-gray-light">
