f91b070f6fcc7d1f47d5f109cd739029e1f10d90
kittyapes <kittyapes@gmail.com>
2022-06-13 12:12:46 +0200

    feat: gradient selected uni popup icons

--- src/lib/components/CardPopup/sections/RightSection.svelte
+++ src/lib/components/CardPopup/sections/RightSection.svelte
@@ -28,7 +28,7 @@
 	<div class="flex flex-grow-0 space-x-6">
 		{#each tabs.filter((t) => t.visible) as tab}
 			<button class="flex items-center space-x-2 btn" on:click={() => (selectedTab = tab)}>
-				<img src={getIconUrl('card-popup-tab-icon/' + tab.icon)} alt={tab.text} />
+				<img src={getIconUrl('card-popup-tab-icon/' + tab.icon + (tab === selectedTab ? '.selected' : ''))} alt={tab.text} />
 				<div class="text-[#8C8C8C]" class:gradient-text={tab === selectedTab}>{tab.text}</div>
 			</button>
 		{/each}
