1d2c9fd657947edbae15d20a5410a221d87d1a05
kittyapes <109080669+kittyapes@users.noreply.github.com>
2022-06-14 10:57:21 +0300

    feat: profile dropdown + my collections route

---
+++ src/routes/profile/[address]/collections.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div class="user-collections">
+	<h1 class="mx-auto">Work in Progress</h1>
+	<p>
+		Showing collections from user: {$page.params.address}
+	</p>
+</div>
