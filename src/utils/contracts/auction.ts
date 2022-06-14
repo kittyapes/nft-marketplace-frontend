353d915e19c096ecec6f40373fd971b0b32fd48b
kittyapes <kittyapes@gmail.com>
2022-06-14 22:42:38 +0200

    feat: add TX wait

--- src/utils/contracts/auction.ts
+++ src/utils/contracts/auction.ts
@@ -1,13 +1,17 @@
 import type { OnChainId } from '$interfaces';
 import type { ContractError } from '$interfaces/contractError';
 import { getSigner } from '$utils/misc/getters';
-import type { BigNumber } from 'ethers';
+import type { BigNumber, ContractTransaction } from 'ethers';
 import { noTryAsync } from 'no-try';
 import HinataMarketplaceContract from './hinataMarketplace';
 
 export async function contractAuctionBid(listingId: OnChainId, amount: BigNumber) {
 	const contract = HinataMarketplaceContract(getSigner());
-	const [err, res] = await noTryAsync(() => contract.bid(listingId, amount));
+	const [err, res]: [any, ContractTransaction] = await noTryAsync(() => contract.bid(listingId, amount));
+
+	if (res) {
+		await res.wait(1);
+	}
 
 	return { err: err as ContractError, res };
 }
@@ -21,7 +25,11 @@ export async function contractGetAuctionBid(listingId: OnChainId) {
 
 export async function contractCompleteAuction(listingId: OnChainId) {
 	const contract = HinataMarketplaceContract(getSigner());
-	const [err, res] = await noTryAsync(() => contract.completeAuction(listingId));
+	const [err, res]: [any, ContractTransaction] = await noTryAsync(() => contract.completeAuction(listingId));
+
+	if (res) {
+		await res.wait(1);
+	}
 
 	return { err: err as ContractError, res };
 }
