3ab1bbfc962419c879e147a6705f5e698da66a5f
kittyapes <109080669+kittyapes@users.noreply.github.com>
2022-06-26 16:16:05 +0300

    feat: fix favorite nfts

--- src/stores/marketplace/index.ts
+++ src/stores/marketplace/index.ts
@@ -1,11 +1,9 @@
-import { currentUserAddress } from '$stores/wallet';
 import type { ListingType } from '$utils/api/listing';
-import { getUserFavoriteNfts } from '$utils/nfts/getUserFavoriteNfts';
-import { derived, writable } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 // popup
 export const selectedCard = writable<any>(); // TODO add types
 
 // Filters
 export const collectionFilters = writable<string>(null);
-export const filters = writable<{status: Set<ListingType>, price: { min: number; max: number }, collection: string }>({status: new Set(['sale']), price: { min: 0, max: 0 }, collection: '' });
+export const filters = writable<{ status: Set<ListingType>; price: { min: number; max: number }; collection: string }>({ status: new Set(['sale']), price: { min: 0, max: 0 }, collection: '' });
--- src/utils/nfts/getUserFavoriteNfts.ts
+++ src/utils/nfts/getUserFavoriteNfts.ts
@@ -2,7 +2,7 @@ import { getApiUrl } from '$utils/api';
 import { getAxiosConfig } from '$utils/auth/axiosConfig';
 import axios from 'axios';
 
-export async function getUserFavoriteNfts(address?: string, page?: number, limit: number = 20) {
-	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { ...getAxiosConfig(address), params: { limit: limit } }).catch((e) => e.response);
+export async function getUserFavoriteNfts(address: string, page: number = 1, limit: number = 50) {
+	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { ...getAxiosConfig(address), params: { limit: limit, address, page } }).catch((e) => e.response);
 	return res?.data.data;
 }
