591efa22425f543c342682fc339d445430ee009d
kittyapes <109080669+kittyapes@users.noreply.github.com>
2022-06-29 17:32:48 +0300

    feat: sort user favorites

--- src/utils/nfts/getUserFavoriteNfts.ts
+++ src/utils/nfts/getUserFavoriteNfts.ts
@@ -4,5 +4,8 @@ import axios from 'axios';
 
 export async function getUserFavoriteNfts(address: string, page: number = 1, limit: number = 50) {
 	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { ...getAxiosConfig(address), params: { limit: limit, address, page } }).catch((e) => e.response);
-	return res?.data.data;
+
+	return res?.data.data.sort((a: any, b: any) => {
+		return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+	});
 }
