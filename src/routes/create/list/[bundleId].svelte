8d5f6560fa4c3ee388ae6c7410b8a44b86764d1e
kittyapes <kittyapes@gmail.com>
2022-07-02 20:13:52 +0200

    fix: no error handler for listing NFT

--- src/routes/create/list/[bundleId].svelte
+++ src/routes/create/list/[bundleId].svelte
@@ -14,6 +14,7 @@
 	import { createListingFlow, type CreateListingFlowOptions } from '$utils/flows/createListingFlow';
 	import { contractGetTokenAddress, getTokenAddress } from '$utils/misc/getTokenAddress';
 	import { goBack } from '$utils/navigation';
+	import { notifyError } from '$utils/toast';
 	import dayjs from 'dayjs';
 	import { BigNumber } from 'ethers';
 	import { parseUnits } from 'ethers/lib/utils.js';
@@ -86,6 +87,10 @@
 		}
 
 		const { err, success } = await createListingFlow(flowOptions);
+
+		if (err) {
+			notifyError('Failed to list NFT!');
+		}
 
 		if (success) {
 			goto('/profile/' + $currentUserAddress + '?tab=listings');
