5d32998cf7d8d94aa4c65596dc1d52e43efac2fb
PavelLukl <pavellukl@ids.company>
2022-07-06 19:17:11 +0200

    fix: send correct royalties to contract

--- src/utils/api/management/whitelistCollection.ts
+++ src/utils/api/management/whitelistCollection.ts
@@ -9,7 +9,6 @@ import { getApiUrl } from '..';
 
 export const whitelistCollection = async (address: string) => {
 	const [err, res] = await noTryAsync(() => axios.post(getApiUrl('latest', 'collections/crawl-collection'), { address }, getAxiosConfig()));
-
 	if (err) {
 		notifyError('Failed to whitelist collection');
 		throw new Error('Failed to whitelist collection');
@@ -17,14 +16,12 @@ export const whitelistCollection = async (address: string) => {
 
 	const factoryContract = getContract('factory');
 
-	let royalties = [];
-
-	royalties = res.data.data.royalties;
+	let royalties = [{fees: res.data.data.metadata.dev_seller_fee_basis_points / 10, address: res.data.data.metadata.payout_address}];
 	
 	let beneficiaries = royalties.length > 0 ? royalties.map((item) => item.address) : ['0x0000000000000000000000000000000000000000'];
 	let percentages = royalties.length > 0 ? royalties.map((item) => parseUnits(item.fees.toString(), 2)) : [parseUnits('0', 2)];
-
-	await contractCaller(factoryContract, 'register', 150, 1, res.data.data.collectionAddress, beneficiaries, percentages);
+	
+	await contractCaller(factoryContract, 'register', 150, 1, res.data.data.collectionAddress, beneficiaries, percentages).catch(() => notifyError('Failed to whitelist collection on-chain'));
 
 	return res.data.data;
 };
