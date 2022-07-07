4b6fd2c6ccd072d4987a033e8a31903910cb6418
kittyapes <109080669+kittyapes@users.noreply.github.com>
2022-07-07 19:00:34 +0300

    feat: breaking change that fixes auth (potentially)

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -1,35 +1,25 @@
-import { currentUserAddress, connectionDetails } from '$stores/wallet';
+import { connectionDetails, currentUserAddress } from '$stores/wallet';
 import { get } from 'svelte/store';
-
-const LATEST = 2;
-const DATABASE_ITERATION = 2;
 
 export type ApiVersion = 'latest' | `sprint-${number}` | 'v2';
 
 export function getApiUrl(apiVersion: ApiVersion, apiPath: string): string {
-	let domain = null;
+	// defaults to override
+	const chainId = get(currentUserAddress) ? get(connectionDetails)?.chainId ?? 'OVERRIDE' : +import.meta.env.VITE_DEFAULT_NETWORK || 'OVERRIDE';
 
-	const shouldUseMain = get(connectionDetails)?.chainId === 1 || !get(currentUserAddress);
+	let apiUrl = import.meta.env[`VITE_${chainId}_API_URL`] as string;
 
-	if (shouldUseMain) {
-		domain = 'https://api.hinata.io/api/v1';
-	} else {
-		let overrideApiUrl = import.meta.env.VITE_OVERRIDE_API_URL as string;
-
-		if (overrideApiUrl) {
-			overrideApiUrl = overrideApiUrl.replace(/\/+$/, '');
-
-			console.warn(`[API] Ignoring '${apiVersion}' version and overriding the API URL with: "${overrideApiUrl}"`);
-			domain = overrideApiUrl;
-		} else if (apiVersion === 'v2') {
-			domain = 'https://hinata-test-v2.rekt-news.xyz/api/v2';
-		} else if (apiVersion === 'latest') {
-			domain = `https://hinata-test-v${LATEST}.rekt-news.xyz/api/v${DATABASE_ITERATION}`;
-		} else if (apiVersion.match(/^sprint-\d+$/)) {
-			const versionNumber = apiVersion.match(/^sprint-(\d+)$/)[1];
-			domain = `https://hinata-test-v${versionNumber}.rekt-news.xyz/api/v${DATABASE_ITERATION}`;
-		}
+	if (!apiUrl) {
+		throw new Error(`VITE_${chainId}_API_URL env variable not set!`);
 	}
 
-	return `${domain}/${apiPath}`;
+	if (apiUrl.endsWith('/')) {
+		apiUrl = apiUrl.replace(/\/$/, '');
+	}
+
+	if (!apiPath.startsWith('/')) {
+		apiPath = '/' + apiPath;
+	}
+
+	return apiUrl + apiPath;
 }
--- src/utils/auth/token.ts
+++ src/utils/auth/token.ts
@@ -1,3 +1,4 @@
+import { getApiUrl } from '$utils/api';
 import { isJwtExpired } from '$utils/jwt';
 import { getAddress } from '$utils/misc/getters';
 
@@ -10,7 +11,7 @@ function getAuthTokenKey(address: string) {
 		throw new Error('No address provided and could not automatically get an address.');
 	}
 
-	return `authToken-${address}`;
+	return `authToken-${address}-${getApiUrl('latest', '')}`;
 }
 
 export function getAuthToken(address?: string) {
