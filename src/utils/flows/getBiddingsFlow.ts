6e924fd43572fb3685c67dbc5f5426061e419fd1
kittyapes <kittyapes@gmail.com>
2022-08-01 15:25:36 +0200

    fix: refresh bids error handling

--- src/utils/flows/getBiddingsFlow.ts
+++ src/utils/flows/getBiddingsFlow.ts
@@ -1,5 +1,6 @@
 import { getApiUrl } from '$utils/api';
 import { contractGetAuctionBid } from '$utils/contracts/auction';
+import { notifyError } from '$utils/toast';
 import axios from 'axios';
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration.js';
@@ -29,7 +30,7 @@ async function fetchHighestBid(listingId: string) {
 	return highestBid;
 }
 
-export async function getBiddingsFlow(listingId: string, tokenDecimals: number): Promise<BidRow[]> {
+async function _fetchBiddings(listingId: string, tokenDecimals: number) {
 	const res = await axios.get(getApiUrl('latest', 'listings/' + listingId + '/bids'));
 	const bids = res.data.data as any[];
 
@@ -61,4 +62,15 @@ export async function getBiddingsFlow(listingId: string, tokenDecimals: number):
 	}));
 
 	return adaptedBids;
+}
+
+export async function getBiddingsFlow(listingId: string, tokenDecimals: number): Promise<BidRow[]> {
+	try {
+		return await _fetchBiddings(listingId, tokenDecimals);
+	} catch (err) {
+		console.error(err);
+		notifyError('Something went wrong while trying to refresh bids.');
+
+		return [];
+	}
 }
