f23c9a72150c73789344575abb64a341c1f56b9d
kittyapes <kittyapes@gmail.com>
2022-08-04 16:53:21 +0300

    fix: HINATA-1091 + UI Updates

--- src/lib/components/Progressbar.svelte
+++ src/lib/components/Progressbar.svelte
@@ -1,24 +1,28 @@
 <script lang="ts">
 	export let value: number;
-	export let points: { at: number; label: string; dot?: boolean }[] = [];
+	export let points: { at: number; label: string; dot?: boolean; top_value: string }[] = [];
 </script>
 
-<div class="relative">
-	<div id="rail" class="h-4 w-full rounded-full bg-[#c4c4c4] relative {$$props.class}" style="--after-width: {value}%" />
+<div class="relative mt-8">
+	<div id="rail" class="h-4 w-full rounded-full bg-[#c4c4c4] relative {$$props.class}" style="--after-width: {value === 80 ? 100 : value}%" />
 
 	{#each points as point}
+		<div
+			class="-translate-x-1/2 uppercase text-xs text-center font-light absolute -top-[150%] whitespace-nowrap"
+			class:gradient-text={value >= point.at || (point.at === 100 && value === 80)}
+			style="left: {point.at}%"
+		>
+			{point.top_value}
+		</div>
 		<div class="absolute top-0" style="left: {point.at}%">
 			<div
 				class="w-6 h-6 rounded-full
-					{value >= point.at ? 'bg-gradient-to-r from-color-purple to-color-blue' : 'bg-[#c4c4c4]'}
+					{value >= point.at || (point.at === 100 && value === 80) ? 'bg-gradient-to-r from-color-purple to-color-blue' : 'bg-[#c4c4c4]'}
 					-translate-x-1/2 translate-y-[-4px]"
 				class:opacity-0={point.dot == false}
 			/>
 
-			<div
-				class="-translate-x-1/2 uppercase text-sm text-center max-w-min font-light
-					{value >= point.at ? 'gradient-text' : ''}"
-			>
+			<div class="-translate-x-1/2 uppercase text-xs italic text-center font-light" class:gradient-text={value >= point.at}>
 				{point.label}
 			</div>
 		</div>
