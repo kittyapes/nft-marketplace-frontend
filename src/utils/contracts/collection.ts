9f301d79c1d62a26852b5604dee445f82fce683b
kittyapes <kittyapes@gmail.com>
2022-08-17 22:38:31 +0300

    feat: update spawn function

--- src/utils/contracts/collection.ts
+++ src/utils/contracts/collection.ts
@@ -22,12 +22,11 @@ export async function contractCreateCollection(options: {
 
 	const res = await contractCaller(
 		contract,
-		'spawn',
+		'create',
 		150,
 		1,
 		options.name,
 		options.paymentTokenTicker,
-		options.slug,
 		options.royalties.length > 0 ? options.royalties.map((item) => item.address) : ['0x0000000000000000000000000000000000000000'],
 		options.royalties.length > 0 ? options.royalties.map((item) => ethers.utils.parseUnits(item.fees.toString(), 2)) : [ethers.utils.parseUnits('0', 2)],
 		false
