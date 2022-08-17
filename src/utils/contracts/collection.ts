16731c9a6310d5d3d882da7848ceb8f22b8ad4ab
kittyapes <kittyapes@gmail.com>
2022-08-17 22:37:42 +0300

    feat: update staging

--- src/utils/contracts/collection.ts
+++ src/utils/contracts/collection.ts
@@ -22,16 +22,25 @@ export async function contractCreateCollection(options: {
 
 	const res = await contractCaller(
 		contract,
-		'spawn',
+		'create',
 		150,
 		1,
 		options.name,
 		options.paymentTokenTicker,
-		options.slug,
 		options.royalties.length > 0 ? options.royalties.map((item) => item.address) : ['0x0000000000000000000000000000000000000000'],
 		options.royalties.length > 0 ? options.royalties.map((item) => ethers.utils.parseUnits(item.fees.toString(), 2)) : [ethers.utils.parseUnits('0', 2)],
 		false
-	);
+	).catch((err) => {
+		console.log(
+			options.name,
+			options.paymentTokenTicker,
+			options.slug,
+			options.royalties.length > 0 ? options.royalties.map((item) => item.address) : ['0x0000000000000000000000000000000000000000'],
+			options.royalties.length > 0 ? options.royalties.map((item) => ethers.utils.parseUnits(item.fees.toString(), 2)) : [ethers.utils.parseUnits('0', 2)],
+			false,
+			err
+		);
+	});
 
 	return { contractAddress: res.events[0].args[2] };
 }
