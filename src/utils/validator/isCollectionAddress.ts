6abf0e549867f5500a288e19cc20ec78e1384f34
kittyapes <kittyapes@gmail.com>
2022-09-02 19:46:06 +0300

    feat: collection whitelisting

--- src/utils/validator/isCollectionAddress.ts
+++ src/utils/validator/isCollectionAddress.ts
@@ -1,16 +1,28 @@
 import { appProvider } from '$stores/wallet';
 import { getContractInterface } from '$utils/contracts/collection';
+import { ethers } from 'ethers';
 import { get } from 'svelte/store';
 import { isContractAddress } from './isContractAddress';
 
-export default async function (address: string) {
+export default async function (address: string, networkId?: number) {
+	// In some cases we may wanna specify the network ID, here we decide which app provider to use
+	// based on whether a network ID is provided or not
+	let provider: ethers.providers.BaseProvider;
+
+	if (networkId !== undefined) {
+		provider = ethers.getDefaultProvider(networkId);
+	} else {
+		// Checks mainnet by default if no wallet is connected
+		provider = get(appProvider) ?? ethers.getDefaultProvider(+import.meta.env.VITE_DEFAULT_NETWORK);
+	}
+
 	try {
 		// check if contract address
-		const is_contract = await isContractAddress(address).catch(() => false);
+		const is_contract = await isContractAddress(address, provider).catch(() => false);
 
 		// check if erc1155
 		// check if erc721
-		const contract_interface = await getContractInterface(address, get(appProvider));
+		const contract_interface = await getContractInterface(address, provider);
 
 		if (is_contract) {
 			return {
