1979f5924a3bb491d5a8e7884862cb7fa20f2e62
PavelLukl <pavellukl@ids.company>
2022-09-19 17:46:44 +0200

    fix: preventing potential server problems

--- src/routes/collections/[collectionSlug]/+page.js
+++ src/routes/collections/[collectionSlug]/+page.js
@@ -7,8 +7,8 @@ import { get } from 'svelte/store';
 export async function load(event) {
 	const slug = event.params.collectionSlug;
 
-	const res = await axios.get(getApiUrl('latest', 'collections/' + slug), { params: { limit: 1, page: 1 } });
-	const collection = res.data.data;
+	const res = await axios.get(getApiUrl('latest', 'collections/' + slug), { params: { limit: 1, page: 1 } }).catch();
+	const collection = res?.data?.data;
 
 	return {
 		collection,
