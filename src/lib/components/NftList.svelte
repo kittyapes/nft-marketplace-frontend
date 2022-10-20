eeb96b246febcfe050423c1b5de325a4a8d541f1
PavelLukl <pavellukl@ids.company>
2022-10-20 18:31:15 +0200

    feat: make create nft button on collection screen permament

--- src/lib/components/NftList.svelte
+++ src/lib/components/NftList.svelte
@@ -4,6 +4,9 @@
 	import { inview } from 'svelte-inview';
 	import { createEventDispatcher } from 'svelte';
 	import type { CardOptions } from '$interfaces/ui';
+	import { nftDraft } from '$stores/create';
+	import AddCircle from '$icons/add-circle.svelte';
+	import { goto } from '$app/navigation';
 
 	const dispatch = createEventDispatcher();
 
@@ -11,6 +14,7 @@
 	export let cardPropsMapper: (v: CardOptions) => { options: CardOptions } = (v) => ({ options: v });
 	export let isLoading = false;
 	export let reachedEnd = false;
+	export let createNewNftBtn = { include: false, collectionId: '' };
 
 	const inviewOptions = {};
 
@@ -50,6 +54,22 @@
 
 	{#if options?.length}
 		<div class="nftGrid">
+			{#if createNewNftBtn.include}
+				<div
+					class="grid place-items-center border border-dashed border-opacity-30 border-color-gray-base h-full clickable hover:scale-105 transition-all rounded-2xl max-w-[266px] "
+					on:click={() => {
+						$nftDraft = {};
+						$nftDraft.collectionId = createNewNftBtn.collectionId;
+						goto('/create');
+					}}
+				>
+					<div class="flex flex-col items-center justify-center gap-4">
+						<button class="rounded-full btn">
+							<AddCircle />
+						</button>
+						<div class="text-color-gray-dark">Create a new NFT</div>
+					</div>
+				</div>{/if}
 			{#each options as cardOptions, index (cardOptions.rawResourceData._id)}
 				{#if !hidden.get(cardOptions)}
 					{@const props = cardPropsMapper(cardOptions)}
