993957a99efcc1d264a82b7cdcdcf10ead9f2200
PavelLukl <pavellukl@ids.company>
2022-10-27 13:34:47 +0200

    fix: app freeze on subtab switching

--- src/utils/nfts/getUserFavoriteNfts.ts
+++ src/utils/nfts/getUserFavoriteNfts.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function getUserFavoriteNfts(address: string, page: number = 1, limit: number = 50) {
 	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { params: { limit: limit, address, page } }).catch((e) => e.response);
 
-	return res?.data.data.sort((a: any, b: any) => {
+	return res?.data?.data?.sort((a: any, b: any) => {
 		return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
 	});
 }
