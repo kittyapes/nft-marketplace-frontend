33f9b8af8de69755cf5277eb2b0967624f4e5f95
kittyapes <kittyapes@gmail.com>
2022-10-28 15:54:32 +0200

    fix: remove uppercase from PrimaryButton

--- src/lib/components/v2/ButtonBase/buttonBase.ts
+++ src/lib/components/v2/ButtonBase/buttonBase.ts
@@ -1,1 +1,1 @@
-export const buttonBaseClasses = 'relative w-full uppercase font-semibold active:scale-[98%] disabled:active:scale-100 transition duration-100 cursor-pointer disabled:cursor-auto disabled:opacity-50';
+export const buttonBaseClasses = 'relative w-full font-semibold active:scale-[98%] disabled:active:scale-100 transition duration-100 cursor-pointer disabled:cursor-auto disabled:opacity-50';
