aa96008782393fc59471882c16dda45d1049acfb
kittyapes <kittyapes@gmail.com>
2022-10-31 09:45:37 +0300

    feat: HINATA-1447

--- src/utils/contracts/storage.ts
+++ src/utils/contracts/storage.ts
@@ -3,11 +3,11 @@ import { getContract } from '$utils/misc/getContract';
 async function hasRole(role: 'minter', address: string) {
 	const contract = getContract('storage');
 
-	const _role = await { minter: contract.MINTER_ROLE() }[role];
+	const _role = { minter: await contract.MINTER_ROLE() }[role];
 
 	return await contract.hasRole(_role, address);
 }
 
 export default {
-	hasRole
+	hasRole,
 };
