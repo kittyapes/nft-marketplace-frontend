67a9a552369fbd853098def5e76cdf5500f8fbe9
PavelLukl <pavellukl@ids.company>
2022-11-10 14:32:58 +0100

    fix: profile share button too small

--- src/icons/share-v2.svelte
+++ src/icons/share-v2.svelte
@@ -1,4 +1,4 @@
-<svg width="26" height="30" viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg width="26" height="30" viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg" class={$$props.class}>
 	<path
 		fill-rule="evenodd"
 		clip-rule="evenodd"
