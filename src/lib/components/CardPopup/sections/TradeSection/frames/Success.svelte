07ef123f657d891d976c2fb48da2086fd84578b6
kittyapes <kittyapes@gmail.com>
2022-11-11 21:56:56 +0100

    redesign: unipop success screen

--- src/lib/components/CardPopup/sections/TradeSection/frames/Success.svelte
+++ src/lib/components/CardPopup/sections/TradeSection/frames/Success.svelte
@@ -1,10 +1,26 @@
 <script>
-	import { getIconUrl } from '$utils/misc/getIconUrl';
+	import SuccessCheck from '$icons/success-check.svelte';
+
+	export let message = 'Action performed successfully.';
 </script>
 
-<div class="flex flex-col justify-center h-[90%]">
-	<img class="h-24" src={getIconUrl('gradient-check')} alt="" />
-
-	<div class="font-bold text-2xl text-center text-gradient mt-4">Success!</div>
-	<div class="opacity-50 text-center mt-4">Action performed successfully.</div>
+<div class="flex flex-col justify-center gradient-border-green gap-12 aspect-1">
+	<div class="text-center mt-4 text-3xl text-white">{message}</div>
+	<div class="flex items-center justify-center gap-8">
+		<SuccessCheck />
+		<div class="gradient-text-green text-7xl">Success!</div>
+	</div>
 </div>
+
+<style>
+	.gradient-border-green {
+		border-image: linear-gradient(90deg, #adff00 0%, #4cef00 51.68%, #59ff9c 101.25%) 1;
+		@apply border-solid border-4;
+	}
+
+	.gradient-text-green {
+		@apply text-transparent;
+		background: linear-gradient(90deg, #adff00 0%, #4cef00 51.68%, #59ff9c 101.25%);
+		background-clip: text;
+	}
+</style>
