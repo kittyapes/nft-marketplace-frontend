9cad395b8343270a98ddf9f07ef89c672651b5f2
idsmoura <cleophasbarwareng@ids.company>
2022-11-15 13:00:14 +0300

    masonry layout using columns

--- src/lib/components/v2/CollectionDetail/NFTGrid.svelte
+++ src/lib/components/v2/CollectionDetail/NFTGrid.svelte
@@ -7,6 +7,10 @@
 	import { goto } from '$app/navigation';
 	import DiamondsLoader from '$lib/components/DiamondsLoader.svelte';
 	import Card from './Card.svelte';
+	import { browser } from '$app/environment';
+	import imagesLoaded from 'imagesloaded';
+	import { log } from '$utils/debug';
+	import { bind } from 'svelte/internal';
 	const dispatch = createEventDispatcher();
 
 	export let options: CardOptions[];
@@ -44,6 +48,15 @@
 			}
 		}),
 	);
+
+	$: if (options?.length > 0 && gridStyle === 'masonry' && browser) {
+		const allNftImages = document.getElementsByClassName('nft-image-wrapper');
+		console.log(allNftImages.length, '  TOTAL');
+		for (let i = 0; i < allNftImages?.length; i++) {
+			const nftImage = allNftImages[i];
+			console.log(nftImage.id);
+		}
+	}
 </script>
 
 <div class="w-full text-white">
@@ -52,7 +65,7 @@
 	{/if}
 
 	{#if options?.length || options?.length === 0}
-		<div class:normal-grid={gridStyle === 'normal'} class:dense-grid={gridStyle === 'dense'} class:masonry-grid={gridStyle === 'masonry'}>
+		<div class:normal-grid={gridStyle === 'normal'} class:dense-grid={gridStyle === 'dense'} class:masonry-container={gridStyle === 'masonry'}>
 			{#if createNewNftBtn.include}
 				<div
 					class="grid place-items-center border border-dashed border-opacity-30 border-color-gray-base clickable hover:scale-105 transition-all rounded-2xl max-w-[246px] min-h-[315px]"
@@ -73,7 +86,11 @@
 			{#each options as cardOptions, index (cardOptions.rawResourceData._id)}
 				{#if !hidden.get(cardOptions)}
 					{@const props = cardPropsMapper(cardOptions)}
-					<Card {...props} bind:gridStyle />
+					{#if gridStyle === 'masonry'}
+						<img class="w-full h-auto object-center object-cover mb-4 2xl:mb-5" src={props?.options?.nfts?.[0]?.thumbnailUrl} alt={props?.options?.nfts?.[0]?.name} />
+					{:else}
+						<Card options={props?.options} bind:gridStyle />
+					{/if}
 				{/if}
 			{/each}
 		</div>
@@ -93,28 +110,16 @@
 	.placeholder {
 		@apply p-36 font-semibold text-lg opacity-70;
 	}
-
-	.nftGrid {
-		@apply grid min-w-full gap-3 my-5 mx-auto;
-		grid-template-columns: repeat(auto-fit, 310px);
-	}
 	.normal-grid {
-		@apply grid grid-cols-[repeat(auto-fit,261px)] 2xl:grid-cols-[repeat(auto-fit,261px)];
-		/* @apply grid-cols-4; */
+		@apply grid grid-cols-[repeat(auto-fit,261px)] 2xl:grid-cols-[repeat(auto-fit,324px)];
 		@apply justify-between gap-y-10 2xl:gap-y-14;
-		/* @apply justify-between gap-x-4 2xl:gap-x-20  gap-y-10 2xl:gap-y-14; */
 	}
 	.dense-grid {
-		@apply grid grid-cols-[repeat(auto-fit,166px)] 2xl:grid-cols-[repeat(auto-fit,166px)];
-		/* @apply grid-cols-6; */
+		@apply grid grid-cols-[repeat(auto-fit,166px)] 2xl:grid-cols-[repeat(auto-fit,206px)];
 		@apply justify-between gap-y-8 2xl:gap-y-10;
-		/* @apply justify-between gap-x-5 2xl:gap-x-6 gap-y-8 2xl:gap-y-10; */
 	}
-	.masonry-grid {
-		@apply columns-6;
-		/* @apply grid-cols-[repeat(auto-fit,166px)] 2xl:grid-cols-[repeat(auto-fit,166px)]; */
-		/* @apply grid-cols-6; */
-		/* @apply justify-between gap-y-8 2xl:gap-y-10; */
-		/* @apply justify-between gap-x-5 2xl:gap-x-6 gap-y-8 2xl:gap-y-10; */
+	.masonry-container {
+		@apply columns-6 2xl:columns-[206px];
+		@apply gap-x-5 2xl:gap-x-6;
 	}
 </style>
