f8a15d61b4fd6b951090bbcad21576b1e24d9311
idsmoura <cleophasbarwareng@ids.company>
2022-11-16 17:50:37 +0300

    feat:social links

--- src/icons/socials/deviantart.svelte
+++ src/icons/socials/deviantart.svelte
@@ -1,14 +1,41 @@
-<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" class={$$props.class}>
-	<g clip-path="url(#clip0_5904_29506)">
-		<path
-			d="M17.8855 16.6797L17.9708 22.5751H14.3825C14.7828 20.8722 15.589 19.4994 17.9105 19.1573L17.8855 18.3883C12.7789 19.0555 11.6954 21.8612 11.6235 23.8617L19.8505 23.8568V18.9358C22.3214 18.8724 25.4093 18.9694 26.258 22.5458L22.6029 22.5159V20.0084C21.9484 19.8173 21.3845 19.814 20.7602 19.8574L20.7298 23.8465L28.8861 23.8449C28.9645 19.857 25.4209 18.05 19.8505 18.1358V16.6797H17.8855L17.8855 16.6797Z"
-			fill="white"
-		/>
-	</g>
-	<rect x="0.5" y="0.5" width="39" height="39" stroke="white" />
-	<defs>
-		<clipPath id="clip0_5904_29506">
-			<rect width="17.3503" height="17.3793" fill="white" transform="translate(11.5791 11.5791)" />
-		</clipPath>
-	</defs>
-</svg>
+<script lang="ts">
+	export let gradient = false;
+</script>
+
+{#if gradient}
+	<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" class={$$props.class}>
+		<defs>
+			<linearGradient id="grad" x1="0" x2="100%" y1="0" y2="0">
+				<stop offset="0%" stop-color="#868BF7" />
+				<stop offset="100%" stop-color="#6CC7F8" />
+			</linearGradient>
+		</defs>
+		<g clip-path="url(#clip0_5904_29506)">
+			<path
+				d="M17.8855 16.6797L17.9708 22.5751H14.3825C14.7828 20.8722 15.589 19.4994 17.9105 19.1573L17.8855 18.3883C12.7789 19.0555 11.6954 21.8612 11.6235 23.8617L19.8505 23.8568V18.9358C22.3214 18.8724 25.4093 18.9694 26.258 22.5458L22.6029 22.5159V20.0084C21.9484 19.8173 21.3845 19.814 20.7602 19.8574L20.7298 23.8465L28.8861 23.8449C28.9645 19.857 25.4209 18.05 19.8505 18.1358V16.6797H17.8855L17.8855 16.6797Z"
+				fill="url(#grad)"
+			/>
+		</g>
+		<rect x="0.5" y="0.5" width="39" height="39" stroke="url(#grad)" />
+		<defs>
+			<clipPath id="clip0_5904_29506">
+				<rect width="17.3503" height="17.3793" fill="url(#grad)" transform="translate(11.5791 11.5791)" />
+			</clipPath>
+		</defs>
+	</svg>
+{:else}
+	<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" class={$$props.class}>
+		<g clip-path="url(#clip0_5904_29506)">
+			<path
+				d="M17.8855 16.6797L17.9708 22.5751H14.3825C14.7828 20.8722 15.589 19.4994 17.9105 19.1573L17.8855 18.3883C12.7789 19.0555 11.6954 21.8612 11.6235 23.8617L19.8505 23.8568V18.9358C22.3214 18.8724 25.4093 18.9694 26.258 22.5458L22.6029 22.5159V20.0084C21.9484 19.8173 21.3845 19.814 20.7602 19.8574L20.7298 23.8465L28.8861 23.8449C28.9645 19.857 25.4209 18.05 19.8505 18.1358V16.6797H17.8855L17.8855 16.6797Z"
+				fill="currentColor"
+			/>
+		</g>
+		<rect x="0.5" y="0.5" width="39" height="39" stroke="currentColor" />
+		<defs>
+			<clipPath id="clip0_5904_29506">
+				<rect width="17.3503" height="17.3793" fill="currentColor" transform="translate(11.5791 11.5791)" />
+			</clipPath>
+		</defs>
+	</svg>
+{/if}
--- src/lib/components/v2/CollectionDetail/CollectionDescription.svelte
+++ src/lib/components/v2/CollectionDetail/CollectionDescription.svelte
@@ -8,32 +8,26 @@
 	import type { Collection } from '$utils/api/collection';
 
 	export let collectionData: Collection;
-	let showSocials = true;
+	let showSocials = false;
 	let hoveredSocial = '';
 	let socials: { name?: string; url?: string; component?: any }[] = [];
-	const populateSocials = () => {
-		// if (collectionData?.facebookUrl) {
+	$: if (collectionData?.facebookUrl) {
 		socials.push({ name: 'Facebook', url: collectionData?.facebookUrl, component: Facebook });
-		// }
-		// if (collectionData?.twitterUrl) {
+	}
+	$: if (collectionData?.twitterUrl) {
 		socials.push({ name: 'Twitter', url: collectionData?.twitterUrl, component: Twitter });
-		// }
-		// if (collectionData?.instagramUrl) {
+	}
+	$: if (collectionData?.instagramUrl) {
 		socials.push({ name: 'Instagram', url: collectionData?.instagramUrl, component: Instagram });
-		// }
-		// if (collectionData?.websiteUrl) {
+	}
+	$: if (collectionData?.websiteUrl) {
 		socials.push({ name: 'Website', url: collectionData?.websiteUrl, component: Web });
-		// }
-		// if (collectionData?.deviantartUrl) {
+	}
+	$: if (collectionData?.deviantartUrl) {
 		socials.push({ name: 'Deviantart', url: collectionData?.deviantartUrl, component: Deviantart });
-		// }
-		// if (collectionData?.artstationUrl) {
+	}
+	$: if (collectionData?.artstationUrl) {
 		socials.push({ name: 'Artstation', url: collectionData?.artstationUrl, component: Artstation });
-		// }
-	};
-	$: if (collectionData) {
-		populateSocials();
-		console.log(socials);
 	}
 </script>
 
@@ -50,23 +44,28 @@
 			>
 				<!-- TODO add share icon -->
 			</button>
-			<div class:hidden={!showSocials} class="absolute right-0 z-10 top-12 2xl:top-16 border-gradient py-5 2xl:py-7 px-5 2xl:px-6 bg-dark-gradient flex flex-row items-end gap-x-3 2xl:gap-x-4">
-				<!-- {#if socials?.length > 0} -->
-				{#each socials as social}
-					<div class="flex flex-col items-center gap-y-1 2xl:gap-y-1.5 font-semibold text-[10px] 2xl:text-sm">
-						<p class:hidden={hoveredSocial !== social.name}>{social.name}</p>
-						<a
-							href={social.url}
-							target="_blank"
-							on:mouseenter={() => (hoveredSocial = social.name)}
-							on:mouseleave={() => (hoveredSocial = '')}
-							class="w-8 h-8 2xl:w-9 2xl:h-9 hover:border-gradient border border-white flex items-center justify-center transition-btn"
-						>
-							<svelte:component this={social.component} />
-						</a>
-					</div>
-				{/each}
-				<!-- {/if} -->
+			<div
+				class:hidden={!showSocials}
+				class="absolute right-0 z-10 top-12 2xl:top-16 border-gradient py-5 2xl:py-7 px-5 2xl:px-6 bg-dark-gradient flex flex-row items-end gap-x-3 2xl:gap-x-4 font-semibold text-[10px] 2xl:text-sm"
+			>
+				{#if socials?.length > 0}
+					{#each socials as social}
+						<div class="relative flex flex-col items-center ">
+							<p class:hidden={hoveredSocial !== social.name} class="absolute -top-4 2xl:top-6">{social.name}</p>
+							<a
+								href={social.url}
+								target="_blank"
+								on:mouseenter={() => (hoveredSocial = social.name)}
+								on:mouseleave={() => (hoveredSocial = '')}
+								class="w-8 h-8 2xl:w-9 2xl:h-9 flex items-center justify-center transition-btn {hoveredSocial === social.name ? 'border-gradient' : 'border-white border'}"
+							>
+								<svelte:component this={social.component} gradient={hoveredSocial == social.name} />
+							</a>
+						</div>
+					{/each}
+				{:else}
+					<h2 class="whitespace-nowrap">This collection doesn't have social links.</h2>
+				{/if}
 			</div>
 		</div>
 	</div>
