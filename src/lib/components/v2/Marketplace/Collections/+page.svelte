f275279c8a06ff44e20faa7bb15058617fd8a912
idsmoura <cleophasbarwareng@ids.company>
2022-11-24 11:03:58 +0300

    WIP: collections table

---
+++ src/lib/components/v2/Marketplace/Collections/+page.svelte
@@ -0,0 +1,10 @@
+<table class="w-full    ">
+	<tr>
+		<th class="inline-flex">Collection</th>
+		<th>Total Volume</th>
+		<th>Floor Price</th>
+	</tr>
+	<tbody>
+		<tr />
+	</tbody>
+</table>
--- src/routes/marketplace/+layout.svelte
+++ src/routes/marketplace/+layout.svelte
@@ -4,4 +4,5 @@
 
 <main class="mx-auto text-white pt-28 2xl:pt-32 px-28 2xl:px-36">
 	<Tabs />
+	<slot />
 </main>
