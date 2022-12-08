158adb6f9cf4dad14c8d077b8c9e90784941cd8b
kittyapes <kittyapes@gmail.com>
2022-12-08 15:08:57 +0300

    feat: hide nfts using fullId approach

--- src/lib/components/NftCard.svelte
+++ src/lib/components/NftCard.svelte
@@ -92,7 +92,7 @@
 		dotsOpened = false;
 		ev.stopPropagation();
 
-		const res = await apiHideNft(options.nfts[0].databaseId);
+		const res = await apiHideNft(options.nfts[0].fullId);
 
 		if (res.err) {
 			notifyError('Failed to hide NFT. \n' + res.err.message);
@@ -105,7 +105,7 @@
 		dotsOpened = false;
 		ev.stopPropagation();
 
-		const res = await apiRevealNft(options.nfts[0].databaseId);
+		const res = await apiRevealNft(options.nfts[0].fullId);
 
 		if (res.err) {
 			notifyError('Failed to reveal NFT. \n' + res.err.message);
