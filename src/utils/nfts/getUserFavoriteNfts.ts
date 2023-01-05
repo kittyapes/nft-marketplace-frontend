4eb2a843e04ef0fafea86f2208af57105a683b9b
kittyapes <kittyapes@gmail.com>
2023-01-05 11:27:33 +0300

    feat: user favoriting blocks app fix

--- src/utils/nfts/getUserFavoriteNfts.ts
+++ src/utils/nfts/getUserFavoriteNfts.ts
@@ -2,11 +2,14 @@ import { getApiUrl } from '$utils/api';
 import axios from 'axios';
 
 export async function getUserFavoriteNfts(address: string, page = 1, limit = 50) {
-	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { params: { limit: limit, address, page } }).catch((_e) => null);
+	const res = await axios.get(getApiUrl('latest', 'users/favourite'), { params: { limit: limit, address, page } }).catch((e) => e.response);
 
-	return (
-		res?.data?.data?.sort((a: any, b: any) => {
-			return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-		}) ?? []
-	);
+	if (res.status != 200) {
+		console.error(`Failed to get user liked NFTs. Server returned status code ${res.status}, ${res.statusText}.`);
+		return [];
+	}
+
+	return res?.data?.data?.sort((a: any, b: any) => {
+		return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+	});
 }
