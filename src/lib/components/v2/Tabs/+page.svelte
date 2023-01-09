44682ef46217634d32b74d8b15147af3b8e86831
kittyapes <kittyapes@gmail.com>
2023-01-09 15:30:49 +0100

    fix: marketplace tabs underline

--- src/lib/components/v2/Tabs/+page.svelte
+++ src/lib/components/v2/Tabs/+page.svelte
@@ -7,16 +7,9 @@
 <div class="flex flex-row items-center gap-x-6 2xl:gap-x-8 font-semibold text-xl 2xl:text-2xl leading-5 2xl:leading-6">
 	{#each tabs as tab}
 		{@const selectedRoute = tab?.route?.includes('?') ? tab.route.slice(0, tab.route.indexOf('?')) : tab.route}
-		<button
-			type="button"
-			on:click={() => {
-				goto(tab.route);
-			}}
-			class:text-gradient={selectedRoute === $page.url.pathname}
-			class="flex flex-col gap-y-0.5  p-0"
-		>
+		<button type="button" on:click={() => goto(tab.route)} class:text-gradient={selectedRoute === $page.url.pathname} class="flex flex-col gap-y-0.5 p-0 relative">
 			<span>{tab.title}</span>
-			<div class:hidden={selectedRoute !== $page.url.pathname} class="h-0.5 w-full bg-white" />
+			<div class:opacity-0={selectedRoute !== $page.url.pathname} class="h-0.5 w-full bg-gradient-to-r from-color-blue to-color-purple absolute -bottom-2" />
 		</button>
 	{/each}
 </div>
