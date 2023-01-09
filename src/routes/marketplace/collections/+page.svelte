6e94bcb55e097dd8ad9f54571db1cf06351a9e7e
kittyapes <kittyapes@gmail.com>
2023-01-09 14:19:09 +0100

    refactor: simplify

--- src/routes/marketplace/collections/+page.svelte
+++ src/routes/marketplace/collections/+page.svelte
@@ -1,5 +1,71 @@
-<script>
-	import Collections from '$components/v2/Marketplace/Collections/+page.svelte';
+<script lang="ts">
+	import CollectionsTable from '$components/v2/CollectionsTable/+page.svelte';
+	import { apiSearchCollections, type Collection } from '$utils/api/collection';
+	import { notifyError } from '$utils/toast';
+	import { onMount } from 'svelte';
+	import SortButton from '$components/v2/SortButton/+page.svelte';
+
+	let collections: Collection[] = [];
+	let query: string;
+	let reachedEnd = false;
+	let isLoading = true;
+	let page = 1;
+	const limit = 10;
+
+	const fetchFunction = async () => {
+		const res = await apiSearchCollections({ limit, page });
+		return res.collections;
+	};
+
+	async function fetchMore() {
+		if (reachedEnd || query) return;
+		isLoading = true;
+
+		const res = await fetchFunction();
+
+		if (res.err) {
+			console.error(res.err);
+			notifyError('Failed to fetch more collections.');
+			return;
+		}
+
+		if (res.length === 0) {
+			reachedEnd = true;
+		} else {
+			page++;
+			collections = [...collections, ...res];
+		}
+		isLoading = false;
+	}
+
+	onMount(async () => {
+		await fetchMore();
+	});
+
+	// TODO implement sort options
+	let sortOptions: { title: string; action?: any }[] = [
+		{
+			title: 'Ending soon',
+			action: () => {},
+		},
+		{
+			title: 'Price low to high',
+			action: () => {},
+		},
+		{
+			title: 'Price high to low',
+			action: () => {},
+		},
+		{
+			title: 'Most favorited',
+			action: () => {},
+		},
+	];
 </script>
 
-<Collections />
+<div>
+	<div class="w-full flex flex-row items-center justify-end gap-x-4 my-6 2xl:my-8">
+		<SortButton bind:sortOptions class="h-11 2xl:h-14 max-w-[123px]" />
+	</div>
+	<CollectionsTable bind:collections bind:isLoading on:end-reached={fetchMore} />
+</div>
