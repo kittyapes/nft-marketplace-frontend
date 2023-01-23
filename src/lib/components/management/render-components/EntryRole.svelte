fb8de36e99235adaff864f22457d333df939ab0c
PavelLukl <pavellukl@ids.company>
2023-01-23 13:45:56 +0100

    fix: user management role assignent

--- src/lib/components/management/render-components/EntryRole.svelte
+++ src/lib/components/management/render-components/EntryRole.svelte
@@ -15,20 +15,34 @@
 
 	let localProps;
 
-	$: if (props) {
+	function convertRoleToLabel(role: String) {
+		let res = role;
+
+		if (role === 'superadmin') res = 'sadmin';
+		else if (role === 'inactivated_user' || props.role === 'inactivated') res = 'inactive';
+		else if (role === 'verified_user') res = 'verified';
+		else if (role === 'ACTIVE') res = 'Listed';
+		else if (role === 'INACTIVE') res = 'Inactive';
+
+		return res;
+	}
+
+	function propsToLocalProps(props) {
 		// deep copy
 		localProps = JSON.parse(JSON.stringify(props));
 		localProps.role = localProps.role?.toLowerCase();
 
 		// remove the ability to edit the admin roles if current user isn't superadmin
 		if (!$userHasRole('superadmin')) {
-			localProps.options = localProps.options.slice(1);
+			localProps.options = props.options.slice(1);
 		}
 
 		// change label of displayed role
-		if (props.role === 'superadmin') localProps.role = 'sadmin';
-		else if (props.role === 'inactivated_user' || props.role === 'inactivated') localProps.role = 'inactive';
-		else if (props.role === 'verified_user') localProps.role = 'verified';
+		localProps.role = convertRoleToLabel(props.role);
+	}
+
+	$: if (props) {
+		propsToLocalProps(props);
 	}
 
 	let handleSelect = async (event: CustomEvent) => {
@@ -51,9 +65,11 @@
 				return;
 			}
 
-			localProps.role = getHighestRole([...roles, ...res.roles]);
-			localProps.color = getRoleColor(localProps.role);
-			localProps.arrowGradient = getGradientColors(localProps.role);
+			let highestRole = getHighestRole([...roles, ...res.roles]);
+			localProps.role = convertRoleToLabel(highestRole);
+			localProps.color = getRoleColor(highestRole);
+			localProps.arrowGradient = getGradientColors(highestRole);
+
 			localProps = localProps;
 		} else if (event.detail?.checked) {
 			const [error, res] = await noTryAsync(() => changeCollectionStatus(props.id, event.detail?.value));
@@ -81,5 +97,7 @@
 		dropdownLabel={localProps.role}
 		disableAllOnSelect={localProps.disableAllOnSelect}
 		dispatchAllOptions={localProps.dispatchAllOptions}
+		disabled={props.role === 'superadmin'}
+		disabledOpacity={false}
 	/>
 </ColumnComponentContainer>
