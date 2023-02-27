cc780d85a4667026415994c09891066eb1fa85b1
kittyapes <kittyapes@gmail.com>
2023-02-27 20:53:37 +0100

    first part

---
+++ src/routes/staking-new/ContentClaimVesting.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import GhostButton from '$lib/components/v2/GhostButton.svelte';
+	import PrimaryButton from '$lib/components/v2/PrimaryButton/PrimaryButton.svelte';
+
+	let hinataTokensClaim = 0;
+	let hinataTokensEscrowed = 0;
+	let nextUnlockIn = '29D : 4H : 17M';
+</script>
+
+<div class="grid grid-cols-2">
+	<div>
+		<h2 class="text-lg">Claim Your Rewards</h2>
+		<p class="mt-4">
+			Your Wallet Address is eligible for this airdrop. Please claim your tokens below by submitting
+			a transaction which will pull them to your wallet.
+		</p>
+	</div>
+
+	<div class="grid grid-cols-2 place-items-center gap-4">
+		<h2 class="text-lg">You Will Receive:</h2>
+
+		<div />
+
+		<div>
+			<span class="text-gradient">{hinataTokensClaim}</span>
+			Hinata tokens:
+		</div>
+
+		<PrimaryButton>Claim</PrimaryButton>
+
+		<div>
+			<span class="text-gradient">{hinataTokensEscrowed}</span>
+			Hinata tokens:
+		</div>
+
+		<PrimaryButton>Escrowed</PrimaryButton>
+
+		<div>Next Unlock in:</div>
+
+		<PrimaryButton>{nextUnlockIn}</PrimaryButton>
+	</div>
+</div>
---
+++ src/routes/staking-new/IconEth.svelte
@@ -0,0 +1,26 @@
+<svg width="12" height="17" viewBox="0 0 12 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M11.2645 8.81999L5.94652 11.6484L0.625 8.81999L5.94652 0.871094L11.2645 8.81999ZM5.94652 12.5567L0.625 9.72826L5.94652 16.4763L11.268 9.72826L5.94652 12.5567Z"
+		fill="#67D4F8"
+	/>
+	<path
+		d="M11.2645 8.81999L5.94652 11.6484L0.625 8.81999L5.94652 0.871094L11.2645 8.81999ZM5.94652 12.5567L0.625 9.72826L5.94652 16.4763L11.268 9.72826L5.94652 12.5567Z"
+		fill="url(#paint0_linear_12736_156809)"
+	/>
+	<defs>
+		<linearGradient
+			id="paint0_linear_12736_156809"
+			x1="-3.03011"
+			y1="3.94097"
+			x2="14.0457"
+			y2="-3.71752"
+			gradientUnits="userSpaceOnUse"
+		>
+			<stop stop-color="#A794FF" stop-opacity="0" />
+			<stop offset="0.398438" stop-color="#A794FF" stop-opacity="0.93" />
+			<stop offset="1" stop-color="#8E77F7" stop-opacity="0" />
+			<stop offset="1" stop-color="#8E77F7" stop-opacity="0" />
+			<stop offset="1" stop-color="#A794FF" stop-opacity="0" />
+		</linearGradient>
+	</defs>
+</svg>
