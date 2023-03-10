998a910ce618f4df5cca61d34be5d5775723a0a8
kittyapes <kittyapes@gmail.com>
2023-03-10 17:09:33 +0100

    fix: make staking amount input accept only valid ETH format

--- src/actions/regexFilter.ts
+++ src/actions/regexFilter.ts
@@ -1,3 +1,5 @@
+export const ethAmountRegex = /^(0|[1-9]\d{0,17})(\.(\d{1,18})?)?$/gm;
+
 export function regexFilter(node: HTMLInputElement, options: { regex: RegExp }) {
 	node.addEventListener('keypress', (ev) => {
 		// @ts-ignore
