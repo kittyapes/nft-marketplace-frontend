85b01a2af80fe4a210bf88b3bf99f89d0c5c46b5
kittyapes <kittyapes@gmail.com>
2023-03-14 11:08:02 +0100

    fix: cases where preload function would fail

--- src/lib/components/v2/MediaDisplay/MediaDisplay.svelte
+++ src/lib/components/v2/MediaDisplay/MediaDisplay.svelte
@@ -11,8 +11,12 @@
 	export let objectContain = false;
 
 	export let assetLoaded = false;
+	export let preloadSuccess = null;
 
 	async function preload(src: string): Promise<string> {
+		// TODO fix preloading, it seems like when you open the unipop, the preload works, but when you wanna go into fullscreen, it doesn't
+		console.log('Preload triggered');
+
 		const resp = await fetch(makeHttps(src));
 		const blob = await resp.blob();
 		fileType = blob.type.split('/')[0] as any;
@@ -25,10 +29,12 @@
 				resolve(reader.result as string);
 				assetLoaded = true;
 				loading = false;
+				preloadSuccess = true;
 			};
 
 			reader.onerror = (error) => {
 				loading = false;
+				preloadSuccess = false;
 				reject(`Error: ${error}`);
 			};
 		});
@@ -52,28 +58,47 @@
 
 {#if loading}
 	<Loader />
+{:else if preloadSuccess}
+	{#if fileType === 'video'}
+		<video
+			crossorigin="anonymous"
+			class="max-w-full max-h-full shadow-xl"
+			autoplay
+			loop
+			poster={fallbackAssetUrl}
+		>
+			<source src={assetUrl} type="video/mp4" />
+			<track kind="captions" />
+		</video>
+	{:else}
+		<img
+			src={srcUrl}
+			crossorigin="anonymous"
+			class="{(objectContain && 'object-contain') || 'object-cover'} w-full h-full shadow-xl"
+			alt="Card asset."
+			use:fadeImageOnLoad
+		/>
+	{/if}
 {:else if fileType === 'video'}
-	<video crossorigin="anonymous" class="max-w-full max-h-full shadow-xl" autoplay loop>
-		<source src={assetUrl} type="video/mp4" />
+	<!-- In case the preload function fails to load the data, for example because of a CORS error -->
+	<!-- Prettier formats these ifs in this way for some reason... -->
+	<video
+		crossorigin="anonymous"
+		class="max-w-full max-h-full shadow-xl"
+		poster={fallbackAssetUrl}
+		autoplay
+		loop
+	>
+		<source src={fallbackAssetUrl} type="video/mp4" />
 		<track kind="captions" />
 	</video>
-{:else if fileType === 'image'}
+{:else}
+	<!-- Also an error in preload function -->
 	<img
-		src={srcUrl}
+		src={fallbackAssetUrl}
 		crossorigin="anonymous"
-		class="{(objectContain && 'object-contain') || 'object-cover'} w-full h-full shadow-xl"
+		class="object-cover w-full h-full shadow-xl"
 		alt="Card asset."
 		use:fadeImageOnLoad
 	/>
 {/if}
-
-<!-- {:catch}
-	{#if fileType === 'video'}
-		<video crossorigin="anonymous" class="max-w-full max-h-full shadow-xl" poster={fallbackAssetUrl} autoplay loop>
-			<source src={fallbackAssetUrl} type="video/mp4" />
-			<track kind="captions" />
-		</video>
-	{:else}
-		<img src={fallbackAssetUrl} crossorigin="anonymous" class="object-cover w-full h-full shadow-xl" alt="Card asset." use:fadeImageOnLoad />
-	{/if}
-{/await} -->
