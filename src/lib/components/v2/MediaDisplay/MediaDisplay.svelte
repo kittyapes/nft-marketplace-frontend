863df5881d2e862108ff0fc970da21a12af6fc8d
kittyapes <kittyapes@gmail.com>
2023-03-27 10:59:23 +0300

    feat: remove asset preloads

--- src/lib/components/v2/MediaDisplay/MediaDisplay.svelte
+++ src/lib/components/v2/MediaDisplay/MediaDisplay.svelte
@@ -1,86 +1,47 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
+	// import { browser } from '$app/environment';
 	import Loader from '$icons/loader.svelte';
-	import { fadeImageOnLoad } from '$utils/actions/fadeImageOnLoad';
+	// import { fadeImageOnLoad } from '$utils/actions/fadeImageOnLoad';
 	import { makeHttps } from '$utils/ipfs';
 
-	export let assetUrl: string;
-	export let fallbackAssetUrl: string;
+	export let assetUrl: string | null;
+	export let thumbnailUrl: string;
 	export let srcUrl: string = null;
 	export let fileType: 'image' | 'video' = null;
-	export let objectContain = false;
 
 	export let assetLoaded = false;
 	export let preloadSuccess = null;
 
-	async function preload(src: string): Promise<string> {
-		// TODO fix preloading, it seems like when you open the unipop, the preload works, but when you wanna go into fullscreen, it doesn't
-		let resp: Response;
+	$: loading = true;
 
-		resp = await fetch(makeHttps(src));
-
-		const blob = await resp.blob();
-		fileType = blob.type.split('/')[0] as any;
-
-		return new Promise(function (resolve, reject) {
-			const reader = new FileReader();
-
-			reader.readAsDataURL(blob);
-			reader.onload = () => {
-				resolve(reader.result as string);
-			};
-
-			reader.onerror = (error) => {
-				reject(error);
-			};
-		});
-	}
-
-	// Loading only if a URL containing the image data was not
-	// already provided
-	let loading = !srcUrl;
-
-	async function loadAssetOrFallback() {
-		try {
-			srcUrl = await preload(assetUrl);
-		} catch (err) {
-			console.error(err);
-
-			try {
-				srcUrl = await preload(fallbackAssetUrl);
-			} catch {
-				loading = false;
-				assetLoaded = false;
-				preloadSuccess = false;
-
-				return;
-			}
+	function hasLoaded(hasError: boolean) {
+		if (hasError) {
+			fileType = 'image';
+			srcUrl = thumbnailUrl;
+		} else {
+			fileType = 'video';
+			srcUrl = assetUrl || thumbnailUrl;
 		}
 
+		preloadSuccess = true;
 		loading = false;
 		assetLoaded = true;
-		preloadSuccess = true;
-	}
-
-	$: if (browser && !srcUrl) {
-		loadAssetOrFallback();
 	}
 </script>
 
 <div class="w-full h-full grid place-items-center">
-	{#if loading}
-		<Loader />
-	{:else if fileType === 'video'}
-		<video class="max-w-full max-h-full shadow-xl" autoplay loop poster={fallbackAssetUrl}>
-			<source src={(preloadSuccess && assetUrl) || fallbackAssetUrl} type="video/mp4" />
-			<track kind="captions" />
-		</video>
-	{:else}
-		<img
-			src={(preloadSuccess && srcUrl) || fallbackAssetUrl}
-			class="{(objectContain && 'object-contain') || 'object-cover'} w-full h-full shadow-xl"
-			alt="Card asset."
-			use:fadeImageOnLoad
-		/>
-	{/if}
+	<Loader class={loading ? 'block' : 'hidden'} />
+	<video
+		class="h-full w-auto shadow-xl object-cover"
+		autoplay
+		loop
+		poster={makeHttps(thumbnailUrl)}
+		on:error={() => hasLoaded(true)}
+		on:loadeddata={() => hasLoaded(false)}
+		src={makeHttps(assetUrl || thumbnailUrl)}
+		class:hidden={loading}
+	>
+		<source src={makeHttps(assetUrl || thumbnailUrl)} type="video/mp4" />
+		<track kind="captions" />
+	</video>
 </div>
