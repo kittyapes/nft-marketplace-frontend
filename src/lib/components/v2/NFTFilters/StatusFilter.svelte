e96e1d1093104665b3de62a2cf035cf7b007c00b
PavelLukl <pavellukl@ids.company>
2023-04-25 09:33:52 +0200

    fix: make the listings type marketplace filter behave as in the designs

--- src/lib/components/v2/NFTFilters/StatusFilter.svelte
+++ src/lib/components/v2/NFTFilters/StatusFilter.svelte
@@ -4,28 +4,37 @@
 	import { createEventDispatcher } from 'svelte';
 
 	const dispatch = createEventDispatcher();
-	let filterOptions: { label: string; queryParam: '' | 'auction' | 'sale' | 'not-for-sale' }[] = [
-		{ label: 'All', queryParam: '' },
+
+	let filterOptions: { label: string; queryParam: '' | 'auction' | 'sale' }[] = [
 		{ label: 'Buy Now', queryParam: 'sale' },
 		{ label: 'Auction', queryParam: 'auction' },
 	];
-	let statusFilter = $page?.url?.searchParams.get('types') || filterOptions[0]?.queryParam;
+
+	let statusFilter = $page?.url?.searchParams.get('types') || '';
+
 	$: {
 		if (statusFilter !== '') $page?.url?.searchParams?.set('types', statusFilter);
 		else $page?.url?.searchParams?.delete('types');
+
 		goto(`?${$page?.url?.searchParams}`);
+
 		dispatch('request-refresh');
 	}
 </script>
 
-<div class="grid grid-cols-2 gap-2">
+<div class="flex gap-2">
 	{#each filterOptions as option, index}
 		<button
 			type="button"
 			on:click={() => {
-				statusFilter = option?.queryParam;
+				if (option?.queryParam === statusFilter) {
+					statusFilter = '';
+				} else {
+					statusFilter = option?.queryParam;
+				}
 			}}
-			class="py-2 2xl:py-2.5 font-medium leading-6 2xl:leading-7 border-gradient hover:bg-main-gradient transition-btn flex-grow {option?.queryParam === statusFilter
+			class="py-2 2xl:py-2.5 font-medium leading-6 2xl:leading-7 border-gradient hover:bg-main-gradient transition-btn flex-grow {option?.queryParam ===
+			statusFilter
 				? 'bg-main-gradient'
 				: 'bg-gradient-a'}"
 			class:col-span-2={index === 0}
